01f580a42ddbd41351ba31aa28f3c013e2fcc213
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2017-11-17 18:56:30 +0100

    various doc improves in the actor module

--- src/actor.rs
+++ src/actor.rs
@@ -5,20 +5,22 @@
 // or http://opensource.org/licenses/MIT>, at your option. This file may not be
 // used, copied, modified, or distributed except according to those terms.
 
-//! All actors must implement [`Actor`], the trait that defines how an actor
+//! All actors must implement the [`Actor`] trait, which defines how an actor
 //! handles messages. However the system needs a way to create these actors,
-//! which is defined in the [`NewActor`] trait. A helper struct is provided to
-//! easily implement this trait, see [`ActorFactory`].
+//! which is defined in the [`NewActor`] trait. Helper structs are provided to
+//! easily implement this trait, see [`ActorFactory`] and [`ActorReuseFactory`].
 //!
 //! [`Actor`]: trait.Actor.html
 //! [`NewActor`]: trait.NewActor.html
 //! [`ActorFactory`]: struct.ActorFactory.html
+//! [`ActorReuseFactory`]: struct.ActorReuseFactory.html
 
 use std::mem;
 use std::marker::PhantomData;
 
 use futures::Future;
 
+/// The main actor trait, which defines how an actor handles messages.
 pub trait Actor {
     /// The message that actor can handle.
     ///
@@ -26,11 +28,11 @@ pub trait Actor {
     type Message;
 
     /// An error the actor can return to it's supervisor. This error will be
-    /// consider terminal for this actor and should *not* not be an error of
+    /// considered terminal for this actor and should **not** not be an error of
     /// regular processing of a message.
     ///
-    /// How to process non-terminal errors that happen in regular processing is
-    /// up to the user.
+    /// How to process non-terminal errors that happen during regular processing
+    /// of messages is up to the user.
     // TODO: give advance about how to handle non-terminal errors.
     type Error;
 
@@ -45,11 +47,11 @@ pub trait Actor {
     /// # Note
     ///
     /// The returned future will be completed before another message is handled
-    /// by this actor, effectively blocking this actor until this actor until
-    /// it's completed. If the returned future does any I/O or other blocking
-    /// operations it's recommended to make an actor specific to that blocking
+    /// by this actor, effectively blocking this actor until the future is
+    /// completed. If the returned future does any blocking operations, e.g.
+    /// I/O, it's recommended to make an actor specific to that blocking
     /// operation, e.g. a unqiue actor per request to handle the reading and
-    /// writing of the requests/responses.
+    /// writing of the request/response to a socket.
     fn handle(&mut self, message: Self::Message) -> Self::Future;
 
     // TODO: determine and doc the actor's lifecycle.
@@ -72,7 +74,9 @@ pub trait Actor {
     /// The method that will be called once an actor will be restarted, but just
     /// before actually stopping the actor.
     ///
-    /// The default is to call `post_stop` function.
+    /// The default is to call the [`post_stop`] function.
+    ///
+    /// [`post_stop`]: trait.Actor.html#method.post_stop
     fn pre_restart(&mut self) {
         self.post_stop();
     }
@@ -80,7 +84,9 @@ pub trait Actor {
     /// The method that will be called once an actor is restarted, but just
     /// before it will accept it's first message.
     ///
-    /// The default is to call `pre_start` function.
+    /// The default is to call the [`pre_start`] function.
+    ///
+    /// [`pre_start`]: trait.Actor.html#method.pre_start
     fn post_restart(&mut self) {
         self.pre_start();
     }
@@ -111,17 +117,16 @@ pub trait NewActor {
     /// [`Actor`]: trait.Actor.html
     type Actor = Actor<Message = Self::Message, Error = Self::Error, Future = Self::Future>;
 
-    /// The method that gets call to create a new actor.
+    /// The method that gets called to create a new actor.
     fn new(&self) -> Self::Actor;
 
     /// Reuse an already allocated actor. The default implementation will create
-    /// a new actor (by calling [`NewActor.new`]), swap the old and new actors
-    /// and drop the `old_actor`.
+    /// a new actor (by calling [`new`]) and replace `old_actor` with it.
     ///
     /// This is a performance optimization to allow the allocations of an actor
     /// to be reused.
     ///
-    /// [`NewActor.new`]: trait.NewActor.html#method.new
+    /// [`new`]: trait.NewActor.html#tymethod.new
     fn reuse(&self, old_actor: &mut Self::Actor) {
         mem::replace(old_actor, self.new());
     }
