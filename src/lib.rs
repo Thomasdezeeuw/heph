2e2b4bbf9240e998658a2275823cfb34d0587bad
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-05-10 13:08:15 +0200

    Enable more warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! TODO: docs.
+
+#![warn(missing_debug_implementations,
+        missing_docs,
+        trivial_casts,
+        trivial_numeric_casts,
+        unused_import_braces,
+        unused_qualifications,
+        unused_results,
+)]
+
 extern crate futures_core;
 
 pub mod actor;
