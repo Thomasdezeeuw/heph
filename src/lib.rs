6c3c4832b85c7dbd7e45d06b2b149349ee4f7a69
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-05-10 11:49:32 +0200

    Remove all outdated code
    We're starting over.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,6 @@
-// Copyright 2017 Thomas de Zeeuw
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
-// or http://opensource.org/licenses/MIT>, at your option. This file may not be
-// used, copied, modified, or distributed except according to those terms.
-
-#![feature(associated_type_defaults)]
-
-extern crate fnv;
-extern crate futures;
-#[macro_use]
-extern crate log;
-extern crate mio;
-extern crate tchannel;
+extern crate futures_core;
 
 pub mod actor;
-pub mod listener;
-pub mod supervisor;
-pub mod system;
 
 /// The actor prelude. All useful traits and structs in single module.
 ///
@@ -26,5 +9,4 @@ pub mod system;
 /// ```
 pub mod prelude {
     pub use actor::{Actor, NewActor, ActorFactory, ReusableActorFactory};
-    pub use system::{ActorSystem, ActorRef};
 }
