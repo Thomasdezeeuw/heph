7391404c6d8866147b63628a479cfd83ab4c024a
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-06-13 23:59:46 +0200

    Add Mailbox to actor_process

---
+++ src/system/actor_process/mailbox.rs
@@ -0,0 +1,56 @@
+use std::collections::VecDeque;
+
+use mio_st::event::Ready;
+use mio_st::registration::Notifier;
+
+use system::error::{SendError, SendErrorReason};
+
+#[derive(Debug)]
+pub struct MailBox<M> {
+    /// The messages in the mailbox.
+    messages: VecDeque<M>,
+    /// The notifier for the process.
+    notifier: Notifier,
+}
+
+impl<M> MailBox<M> {
+    /// Create a new mailbox.
+    ///
+    /// The `Notifier` must be registered with at least `READABLE` interest.
+    pub fn new(mut notifier: Notifier) -> MailBox<M> {
+        debug_assert!(notifier.interests().unwrap().is_readable(),
+            "notifier not registered with `READABLE` interest");
+
+        MailBox {
+            messages: VecDeque::new(),
+            notifier,
+        }
+    }
+
+    /// Get the number of messages.
+    pub fn len(&self) -> usize {
+        self.messages.len()
+    }
+
+    /// Deliver a new message to the mailbox.
+    pub fn deliver(&mut self, msg: M) -> Result<(), SendError<M>> {
+        match self.notifier.notify(Ready::READABLE) {
+            Ok(()) => {
+                self.messages.push_back(msg);
+                Ok(())
+            },
+            Err(_) => {
+                // TODO: detect ActorSystem shutdown.
+                Err(SendError {
+                    message: msg,
+                    reason: SendErrorReason::ActorShutdown,
+                })
+            },
+        }
+    }
+
+    /// Receive a delivered message, if any.
+    pub fn receive(&mut self) -> Option<M> {
+        self.messages.pop_front()
+    }
+}
--- src/system/actor_process/mod.rs
+++ src/system/actor_process/mod.rs
@@ -1,1 +1,5 @@
 //! Module containing the implementation of the `Process` trait for `Actor`s.
+
+mod mailbox;
+
+pub use self::mailbox::MailBox;
