9618e1ebd2d62bcf630e7e4465337043a89f3a78
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-06-13 16:01:33 +0200

    Add a very simple Scheduler
    This is soo simple it doesn't even take the priority of the process into account yet.

--- src/system/scheduler/mod.rs
+++ src/system/scheduler/mod.rs
@@ -1,5 +1,38 @@
 //! Module containing the scheduler.
 
+use system::process::ProcessPtr;
+
 mod priority;
 
 pub use self::priority::Priority;
+
+/// The scheduler, responsible for scheduling and running processes.
+///
+/// The scheduler implements the `Iterator` trait which can used iterate over
+/// all scheduled processes.
+pub struct Scheduler {
+    queue: Vec<ProcessPtr>,
+}
+
+impl Scheduler {
+    /// Create a new scheduler.
+    pub fn new() -> Scheduler {
+        Scheduler {
+            queue: Vec::new(),
+        }
+    }
+
+    /// Schedule a process.
+    pub fn schedule(&mut self, process: ProcessPtr) {
+        // TODO: take priority of the process into account.
+        self.queue.push(process);
+    }
+}
+
+impl<'a> Iterator for &'a mut Scheduler {
+    type Item = ProcessPtr;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.queue.pop()
+    }
+}
