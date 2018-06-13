00503713c697bffae88ffab80cc88b36e1aecc2e
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-06-13 16:24:17 +0200

    Remove Process implementation
    This will be moved to it's own file, but only once it works, at least somewhat works.

--- src/system/process/mod.rs
+++ src/system/process/mod.rs
@@ -1,7 +1,5 @@
-use std::collections::VecDeque;
-use std::marker::PhantomData;
+//! Module containing process related types and implementation.
 
-use actor::Actor;
 use system::scheduler::Priority;
 
 /// Process id, or pid, is an unique id for each process in an `ActorSystem`.
@@ -39,41 +37,3 @@ pub trait Process {
 /// actually cheaper to allocate it on the heap and move around a fat pointer to
 /// it.
 pub type ProcessPtr = Box<dyn Process>;
-
-/// A process that represent an actor, it's mailbox and current execution.
-#[derive(Debug)]
-pub struct Process<'a, A>
-    where A: Actor<'a>,
-{
-    /// Unique id in the `ActorSystem`.
-    id: ProcessId,
-    /// Inbox of the actor.
-    inbox: VecDeque<A::Message>,
-    /// The actor.
-    actor: A,
-
-    // TODO: add ActorOptions, e.g. scheduling Priority.
-}
-
-impl<'a, A> Process<'a, A>
-    where A: Actor<'a>,
-{
-    /// Create a new process.
-        Process {
-    pub(super) fn new(id: ProcessId, actor: A, priority: Priority) -> ActorProcess<'a, A> {
-            id,
-            inbox: VecDeque::new(),
-            actor,
-        }
-    }
-
-    /// Deliver a new message to actor.
-    pub fn deliver(&mut self, msg: A::Message) {
-        self.inbox.push_back(msg)
-    }
-
-    /// Receive a delivered message, if any.
-    pub fn receive(&mut self) -> Option<A::Message> {
-        self.inbox.pop_front()
-    }
-}
