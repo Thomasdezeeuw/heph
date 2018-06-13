2405f3b1d89d978029e2f1113cb8f13354d9b435
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-06-13 16:00:43 +0200

    Add interal Process trait

--- src/system/process/mod.rs
+++ src/system/process/mod.rs
@@ -3,6 +3,25 @@ use std::marker::PhantomData;
 
 use actor::Actor;
 use system::ActorId;
+use system::scheduler::Priority;
+
+/// The trait that represents a process for the `ActorSystem`.
+pub trait Process {
+    // TODO: provided a way to create a futures::task::Context, maybe by
+    // providing an `ActorSystemRef`?
+
+    /// Run the process.
+    ///
+    /// If this function returns it is assumed that the process is:
+    /// - done completely, i.e. it doesn't have to be run anymore, or
+    /// - would block, and it made sure it's scheduled at a later point.
+    fn run(&mut self);
+
+    /// Get the priority of the process.
+    ///
+    /// Used in scheduling the process.
+    fn priority(&self) -> Priority;
+}
 
 /// A process that represent an actor, it's mailbox and current execution.
 #[derive(Debug)]
