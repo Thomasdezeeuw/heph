e6201652ffaf40348aa05554b20e037c07a80155
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-06-15 22:18:57 +0200

    Improve documentation of Hello World example

--- examples/1_hello_world.rs
+++ examples/1_hello_world.rs
@@ -1,11 +1,11 @@
 extern crate actor;
 extern crate futures_core;
 
-use actor::system::{ActorSystemBuilder, ActorOptions};
 use actor::actor::Actor;
 use actor::initiator::NoInitiator;
-use futures_core::{Future, Async, Poll};
+use actor::system::{ActorSystemBuilder, ActorOptions};
 use futures_core::task::Context;
+use futures_core::{Future, Async, Poll};
 
 // Our actor that will greet people and/or things.
 #[derive(Debug)]
@@ -13,49 +13,57 @@ struct GreetingActor {
     message: &'static str,
 }
 
+// To implement the `Actor` trait we need to implement the `Future` trait as
+// well.
 impl Future for GreetingActor {
     // The returned value, must always be empty.
     type Item = ();
     // The type of errors we can generate, in our cause none.
     type Error = ();
 
-    // Since our `handle` implementation always returns `Async::Ready`, this
-    // will never be called.
+    // Since our `handle` method (see Actor implementation below) always returns
+    // `Async::Ready`, this will never be called.
     fn poll(&mut self, _: &mut Context) -> Poll<(), Self::Error> {
         Ok(Async::Ready(()))
     }
 }
 
-// Our Actor implementation.
+// Our `Actor` implementation.
 impl Actor for GreetingActor {
-    // The type of message we can receive.
+    // The type of message we can handle.
     type Message = String;
 
-    // The function that will be called once a message is received.
+    // The function that will be called once a message is received for the actor.
     fn handle(&mut self, _: &mut Context, name: Self::Message) -> Poll<(), Self::Error> {
         // Print a greeting message.
         println!("{} {}", self.message, name);
+        // And that is all we need to done, so we're done.
         Ok(Async::Ready(()))
     }
 }
 
+// Now our actor is ready lets put it to work.
 fn main() {
     // Create our actor.
     let actor = GreetingActor { message: "Hello" };
 
-    // Create a new actor system, which will run the actors.
+    // Create a new actor system, which will run the actor. We'll just use the
+    // default options for the system.
     let mut actor_system = ActorSystemBuilder::default().build()
-        .expect("unable to build `ActorSystem`");
+        .expect("unable to build the actor system");
 
-    // Add our actor to the actor system.
+    // Add our actor to the actor system. For now we'll use the default options
+    // here as well.
     let mut actor_ref = actor_system.add_actor(actor, ActorOptions::default())
-        .expect("unable to add actor to `ActorSystem`");
+        .expect("unable to add actor to actor system");
 
-    // Send our actor a message.
+    // Send our actor a message via an `ActorRef`, which is a reference to the
+    // actor inside the actor system.
     actor_ref.send("World".to_owned())
         .expect("unable to send message");
 
-    // Run our actor system. This should cause "Hello World" to be printed.
+    // Run our actor system. This should cause "Hello World" to be printed and
+    // then it should return.
     actor_system.run::<NoInitiator>(&mut [])
         .expect("unable to run actor system");
 }
