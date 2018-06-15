ef2b26bd7e4255f6366fa7d72e5dd1a3fefa6159
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-06-15 12:02:50 +0200

    Merge all actor module tests into a single file

--- tests/test_actor.rs
+++ tests/test_actor.rs
@@ -1,12 +1,31 @@
-extern crate actor;
-extern crate futures_core;
+//! Tests for the `actor` module.
 
-use actor::actor::{NewActor, actor_factory, reusable_actor_factory};
+use actor::actor::{NewActor, actor_fn, actor_factory, reusable_actor_factory};
 use futures_core::Async;
 
-mod util;
+use util::{TestActor, TestMessage, quick_handle, quick_poll};
 
-use util::{TestActor, TestMessage, quick_handle};
+#[test]
+fn test_actor_fn() {
+    let mut actor_value = 0;
+    {
+        let mut returned_ok = false;
+        let mut actor = actor_fn(|value: usize| {
+            actor_value += value;
+            if !returned_ok {
+                returned_ok = true;
+                Ok(())
+            } else {
+                Err(())
+            }
+        });
+
+        assert_eq!(quick_handle(&mut actor, 1), Ok(Async::Ready(())));
+        assert_eq!(quick_handle(&mut actor, 10), Err(()));
+        assert_eq!(quick_poll(&mut actor), Ok(Async::Ready(())));
+    }
+    assert_eq!(actor_value, 11);
+}
 
 #[test]
 fn test_actor_factory() {
