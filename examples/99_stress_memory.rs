a95e882235956ccccce0dfee009c1b3e6900956f
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-07-20 14:40:03 +0200

    Add memory stress test example

---
+++ examples/99_stress_memory.rs
@@ -0,0 +1,30 @@
+//! This is just a memory stress test of the system.
+//!
+//! Currently using 10 million "actors" this test uses 2.60 GB.
+
+#![feature(never_type)]
+
+extern crate actor;
+extern crate env_logger;
+
+use std::thread;
+use std::time::Duration;
+
+use actor::actor::{Status, actor_fn};
+use actor::system::{ActorSystemBuilder, ActorOptions};
+
+fn main() {
+    let mut actor_system = ActorSystemBuilder::default().build()
+        .expect("unable to build the actor system");
+
+    for _ in 0..10_000_000 {
+        let actor = actor_fn(|_, _: !| -> Result<Status, !> {
+            Ok(Status::Ready)
+        });
+
+        let _ = actor_system.add_actor(actor, ActorOptions::default());
+    }
+
+    println!("Running, check the memory usage!");
+    thread::sleep(Duration::from_secs(100));
+}
