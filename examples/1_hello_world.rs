aff436382588bc666ddb8e96313bb8f949a1a71e
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-08-03 12:16:38 +0200

    Cleanup example 1
    Mostly simplifying the code a bit and improving the comments.

--- examples/1_hello_world.rs
+++ examples/1_hello_world.rs
@@ -1,20 +1,10 @@
 #![feature(async_await, await_macro, futures_api, never_type)]
 
-use std::borrow::Cow;
-
 use heph::actor::{ActorContext, actor_factory};
 use heph::system::{ActorSystemBuilder, ActorOptions};
 
-/// The type of message our actor can receive.
-type ActorMessage = Cow<'static, str>;
-
 /// Our greeter actor.
-///
-/// This function actually implements the `NewActor` trait required by
-/// `TcpListener` (see main). This is the reason why we write the strange
-/// `(stream, address)` form in the arguments.
-async fn greeter_actor(mut ctx: ActorContext<ActorMessage>, message: &'static str) -> Result<(), !> {
-    // Try to receive a name and print it.
+async fn greeter_actor(mut ctx: ActorContext<&'static str>, message: &'static str) -> Result<(), !> {
     let name = await!(ctx.receive());
     println!("{} {}", message, name);
     Ok(())
@@ -29,14 +19,15 @@ fn main() {
     let mut actor_system = ActorSystemBuilder::default().build()
         .expect("unable to build the actor system");
 
-    // Create a new actor factory. This is used to create a new actor on each
-    // thread, although we'll only start a single thread.
+    // Create a new actor factory. This is used to implement the `NewActor`
+    // trait.
     let new_actor = actor_factory(greeter_actor);
-    // Add our actor to the actor system, along with the starting item, in our
-    // case a message for the greeter. We'll use the default options here as
-    // well.
+    // Add our actor to the actor system, along with the starting item. We'll
+    // use the default options here as well.
     let mut actor_ref = actor_system.add_actor(new_actor, "Hello", ActorOptions::default());
 
+    // By default actor don't do anything when adding to the actor system. We
+    // need to wake them, for example by sending them a message.
     // Send our actor a message via an `ActorRef`, which is a reference to the
     // actor inside the actor system.
     actor_ref.send("World")
