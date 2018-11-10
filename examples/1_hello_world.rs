852a5ea5d3f9b2408f5b15f4f0705fba5711b938
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-11-10 20:57:45 +0100

    Update example to use supervisors

--- examples/1_hello_world.rs
+++ examples/1_hello_world.rs
@@ -3,6 +3,7 @@
 use std::io;
 
 use heph::actor::{ActorContext, actor_factory};
+use heph::supervisor::NoopSupervisor;
 use heph::system::{ActorSystem, ActorSystemRef, ActorOptions};
 
 /// Our greeter actor.
@@ -20,14 +21,19 @@ fn add_greeter_actor(mut system_ref: ActorSystemRef) -> io::Result<()> {
     // trait.
     let new_actor = actor_factory(greeter_actor);
 
-    // Add our actor to the actor system, along with the starting item. We'll
-    // use the default actor options here.
-    let mut actor_ref = system_ref.add_actor(new_actor, "Hello", ActorOptions::default());
+    // Add our actor to the actor system.
+    // All actors need supervision, however our actor doesn't return an error
+    // (it uses `!` as error type), because of this we'll use the
+    // `NoopSupervisor`, which is a no-op supervisor.
+    // Along with the supervisor we'll also supply the argument to start the
+    // actor, in our case the greeting message.
+    // We'll use the default actor options here.
+    let mut actor_ref = system_ref.add_actor(NoopSupervisor, new_actor, "Hello", ActorOptions::default());
 
     // By default actor don't do anything when added to the actor system. We
     // need to wake them, for example by sending them a message.
-    // Send our actor a message via an `LocalActorRef`, which is a reference to
-    // the actor inside the actor system.
+    // So we'll send our actor a message via an `LocalActorRef`, which is a
+    // reference to the actor inside the actor system.
     actor_ref.send("World")
         .expect("unable to send message");
 
