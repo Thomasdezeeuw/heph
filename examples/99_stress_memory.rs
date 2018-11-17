272d8d67624ace73dc7b753017b5ae775b18eb18
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-11-17 14:02:33 +0100

    Remove ActorFactory usage from examples

--- examples/99_stress_memory.rs
+++ examples/99_stress_memory.rs
@@ -7,12 +7,12 @@
 use std::thread;
 use std::time::Duration;
 
-use heph::actor::{ActorContext, actor_factory};
+use heph::actor::ActorContext;
 use heph::supervisor::NoopSupervisor;
 use heph::system::{ActorSystem, ActorOptions};
 
 /// Our "actor", but it doesn't do much.
-async fn actor(_: ActorContext<!>, _: ()) -> Result<(), !> {
+async fn actor(_: ActorContext<!>) -> Result<(), !> {
     Ok(())
 }
 
@@ -20,8 +20,7 @@ fn main() {
     ActorSystem::new()
         .with_setup(|mut system_ref| {
             for _ in 0..10_000_000 {
-                let new_actor = actor_factory(actor);
-                let _ = system_ref.spawn(NoopSupervisor, new_actor, (), ActorOptions::default());
+                let _ = system_ref.spawn(NoopSupervisor, actor as fn(_) -> _, (), ActorOptions::default());
             }
 
             println!("Running, check the memory usage!");
