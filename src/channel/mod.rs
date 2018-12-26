f5e54e1ea7a4ff104819188aed6d1af126e3d02d
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-12-26 14:26:36 +0100

    Document the three channel flavours

--- src/channel/mod.rs
+++ src/channel/mod.rs
@@ -1,12 +1,48 @@
 //! Channel communication primitives.
 //!
-//! All these channels are **not** thread safe, they don't implement [`Send`] or
-//! [`Sync`]. If thread safe channels are required the standard library provides
-//! channels in the [`std::sync::mpsc`] modules, or look at
-//! [`crossbeam_channels`].
+//! These channels come in three flavours:
+//!
+//! - *One shot*, a channel that allows a single value to be send across.
+//! - *Bounded*, a channel that buffers a maximum number of values in the
+//!   channel.
+//! - *Unbounded*, a channel with a buffer that grows infinitely.
+//!
+//! All three channels flavours are **not** thread safe, they don't implement
+//! [`Send`] or [`Sync`]. If thread safe channels are required the standard
+//! library provides a channel in the [`std::sync::mpsc`] module, or look at the
+//! [`crossbeam_channels`] crate.
+//!
+//! All three flavours follow roughly the same API. Each submodule has a
+//! `channel` function that can be used to create the `Sender` and `Receiver`
+//! halves of the channel. Alternatively the [`oneshot`], [`bounded`] and
+//! [`unbounded`] functions can be used to create the two halves. Each `Sender`
+//! has a `send` method, which sends a value across the channel. Each `Receiver`
+//! either implements `Future` or `Stream` to receive values from the channel.
+//! The bounded and unbounded flavours also have a `receive_one` method that
+//! returns a `Future` that receives a single value from the channel.
 //!
 //! [`crossbeam_channels`]: https://crates.io/crates/crossbeam-channel
 //! [`std::sync::mpsc`]: https://doc.rust-lang.org/nightly/std/sync/mpsc/index.html
+//! [`oneshot`]: fn.oneshot.html
+//! [`bounded`]: fn.bounded.html
+//! [`unbounded`]: fn.unbounded.html
+//!
+//! # When to use which flavour
+//!
+//! When you expect a maximum of one value to be send across the channel the
+//! answer is simple, use the [oneshot] channel. For example when using the
+//! [request-response pattern].
+//!
+//! When more then one value is expected to be send the choice gets a little
+//! harder. If you don't want the producer (sender) to be blocked then the
+//! [unbounded] channel is the way to go. However if the consumer (receiver)
+//! can't keep up with the values it might better to limit the pace at which the
+//! producer produces value, in that case a [bounded] channel is the way to go.
+//!
+//! [oneshot]: oneshot/index.html
+//! [request-response pattern]: oneshot/index.html#request-response-pattern
+//! [unbounded]: unbounded/index.html
+//! [bounded]: bounded/index.html
 
 use std::pin::Unpin;
 
