2b2ead4e8bf1b087dbd653fffb78b5868b31edc5
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-12-30 21:29:10 +0100

    Various small cleanups

--- src/mailbox.rs
+++ src/mailbox.rs
@@ -43,7 +43,7 @@ impl<M> MailBox<M> {
 
     /// Receive a delivered message, if any.
     pub fn receive(&mut self) -> Option<M> {
-        self.messages2.as_mut()
+        self.messages2.as_ref()
             .and_then(|(_, recv)| recv.try_recv())
             .or_else(|| self.messages.pop_front())
     }
