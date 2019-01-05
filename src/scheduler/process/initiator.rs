3cbd80c3eec52a3d710a7f467aaefe65fa7bb425
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-01-05 14:42:41 +0100

    Cleanup in InitiatorProcess

--- src/scheduler/process/initiator.rs
+++ src/scheduler/process/initiator.rs
@@ -16,8 +16,8 @@ use crate::system::ActorSystemRef;
 /// [`Initiator`]: ../../initiator/trait.Initiator.html
 pub struct InitiatorProcess<I> {
     id: ProcessId,
-    initiator: I,
     runtime: Duration,
+    initiator: I,
 }
 
 impl<I> InitiatorProcess<I> {
@@ -28,8 +28,8 @@ impl<I> InitiatorProcess<I> {
     pub const fn new(id: ProcessId, initiator: I) -> InitiatorProcess<I> {
         InitiatorProcess {
             id,
-            initiator,
             runtime: Duration::from_millis(0),
+            initiator,
         }
     }
 }
@@ -43,7 +43,7 @@ impl<I> Process for InitiatorProcess<I>
 
     fn priority(&self) -> Priority {
         // Initiators always have a low priority, this way requests in progress
-        // are first handled before new requests are accepted and possibly
+        // are handled first before new requests are accepted and possibly
         // overload the system.
         Priority::LOW
     }
@@ -54,15 +54,16 @@ impl<I> Process for InitiatorProcess<I>
 
     fn run(self: Pin<&mut Self>, system_ref: &mut ActorSystemRef) -> ProcessResult {
         trace!("running initiator process: pid={}", self.id);
-
         let start = Instant::now();
+
         // This is safe because we're not moving the initiator.
         let this = unsafe { Pin::get_unchecked_mut(self) };
-        let result = if let Err(err) = this.initiator.poll(system_ref) {
-            error!("error polling initiator: {}", err);
-            ProcessResult::Complete
-        } else {
-            ProcessResult::Pending
+        let result = match this.initiator.poll(system_ref) {
+            Ok(()) => ProcessResult::Pending,
+            Err(err) => {
+                error!("error polling initiator: {}", err);
+                ProcessResult::Complete
+            },
         };
         let elapsed = start.elapsed();
 
