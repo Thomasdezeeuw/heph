7ee22a270f1afccb712ab500df62b882cad82309
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-04-24 14:21:40 +0200

    Update net module documentation
    Removes mention of `ConnectedUdpSocket` and moves most of the docs into the tcp/udp modules.

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,48 +1,18 @@
 //! Network related types.
 //!
-//! # Transmission Control Protocol (TCP)
+//! Two types of socket as provided:
 //!
-//! For TCP two types are provided: a listener and a stream (connection).
+//! * [Transmission Control Protocol] (TCP) module provides two main types:
+//!   * A [TCP stream] between a local and a remote socket.
+//!   * A [TCP socket server], listening for connections.
+//! * [User Datagram Protocol] (UDP) only provides a single socket type:
+//!   * [`UdpSocket`].
 //!
-//! The [`TcpListener`] listens for incoming connections and accepts them
-//! starting a new actor for each accepted connection.
-//!
-//! [`TcpStream`] on the other hand represent a single TCP connection. It can
-//! either be a connection accepted by the `TcpListener`, or one started by
-//! [connecting] to a remote address. It implements the `AsyncRead` and
-//! `AsyncWrite` trait to work nicely with futures.
-//!
-//! [`TcpListener`]: crate::net::TcpListener
-//! [connecting]: crate::net::TcpStream::connect
-//! [`TcpStream`]: crate::net::TcpStream
-//!
-//! # User Datagram Protocol (UDP)
-//!
-//! Two UDP socket types are provided: connected and unconnected. If you're
-//! familiar with UDP you know that UDP doesn't have a concept of a connection,
-//! you send packet across the internet and hope it end up at its destination
-//! without many guarantees. However in some cases a back and forth
-//! communication is still required, in those cases the [`ConnectedUdpSocket`]
-//! type can be used.
-//!
-//! `ConnectedUdpSocket` is a socket that is bound to a local address and is
-//! connected to a remote address. This allows it use [`poll_send`] and
-//! [`poll_recv`] rather then [`poll_send_to`] in which you also need to
-//! specific the address to send the packet to, this makes it easier to use.
-//!
-//! However UDP sockets don't need to be connected to a single remote address.
-//! It can also be used to receive from and send to a number of different remote
-//! addresses. Then [`UdpSocket`] can be used, this is a UDP socket that only
-//! bound to a local address. It has the [`poll_send_to`] method that allows
-//! sending to any remote address and [`poll_recv_from`] to receive packets from
-//! any address.
-//!
-//! [`ConnectedUdpSocket`]: crate::net::ConnectedUdpSocket
-//! [`poll_send`]: crate::net::ConnectedUdpSocket::poll_send
-//! [`poll_recv`]: crate::net::ConnectedUdpSocket::poll_recv
-//! [`poll_send_to`]: crate::net::UdpSocket::poll_send_to
+//! [Transmission Control Protocol]: crate::net::tcp
+//! [TCP stream]: crate::net::TcpStream
+//! [TCP socket server]: crate::net::TcpListener
+//! [User Datagram Protocol]: crate::net::udp
 //! [`UdpSocket`]: crate::net::UdpSocket
-//! [`poll_recv_from`]: crate::net::UdpSocket::poll_recv_from
 
 /// A macro to try an I/O function.
 ///
