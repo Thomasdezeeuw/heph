1484ef9e03fcb361eb21004b48d4bc42a4e514ec
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-06-18 16:01:53 +0200

    Enable IPv6 on Travis

---
+++ .ci/clippy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+# Handy when debugging problems.
+cargo --version
+rustc --version
+
+rustup component add clippy
+
+# Failing the `cognitive-complexity' lint is allowed because the tests are too
+# complex.
+cargo clippy --all-targets --all-features -- -D warnings -A clippy::cognitive-complexity
