40ce762671f750227ee806564ababd1eb252672d
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-08-11 12:54:01 +0200

    Expand tests for UdpSocket::connecting
    Add a test that tests the initial connecting, connect an already
    connected socket to the same address and to a different address.

--- src/net/tests.rs
+++ src/net/tests.rs
@@ -132,6 +132,79 @@ async fn connected_udp_actor(
     Ok(())
 }
 
+#[test]
+fn reconnecting_udp_socket_ipv4() {
+    test_reconnecting_udp_socket(any_local_address())
+}
+
+#[test]
+fn reconnecting_udp_socket_ipv6() {
+    test_reconnecting_udp_socket(any_local_ipv6_address())
+}
+
+fn test_reconnecting_udp_socket(local_address: SocketAddr) {
+    let local_address = SocketAddr::new(local_address.ip(), 0);
+    let socket1 = std::net::UdpSocket::bind(local_address).unwrap();
+    let socket2 = std::net::UdpSocket::bind(local_address).unwrap();
+
+    let address1 = socket1.local_addr().unwrap();
+    let address2 = socket2.local_addr().unwrap();
+
+    #[allow(trivial_casts)]
+    let reconnecting_actor = reconnecting_actor as fn(_, _, _) -> _;
+    let (actor, _) = init_actor(reconnecting_actor, (address1, address2)).unwrap();
+    pin_mut!(actor);
+
+    // Let the actor send all data.
+    loop {
+        match poll_actor(Pin::as_mut(&mut actor)) {
+            Poll::Ready(Ok(())) => break,
+            Poll::Ready(Err(err)) => panic!("unexpected error from actor: {:?}", err),
+            Poll::Pending => sleep(Duration::from_millis(1)),
+        }
+    }
+
+    let mut buf = [0; DATA.len() + 1];
+    // The first socket should receive the data twice.
+    let (bytes_read, peer_address) = socket1.recv_from(&mut buf).unwrap();
+    assert_eq!(bytes_read, DATA.len());
+    assert_eq!(&buf[..DATA.len()], &*DATA);
+    let (bytes_read, peer_address1) = socket1.recv_from(&mut buf).unwrap();
+    assert_eq!(bytes_read, DATA.len());
+    assert_eq!(&buf[..DATA.len()], &*DATA);
+    assert_eq!(peer_address, peer_address1);
+
+    let (bytes_read, peer_address1) = socket2.recv_from(&mut buf).unwrap();
+    assert_eq!(bytes_read, DATA.len());
+    assert_eq!(&buf[..DATA.len()], &*DATA);
+    assert_eq!(peer_address, peer_address1);
+}
+
+async fn reconnecting_actor(
+    mut ctx: actor::Context<!>,
+    peer_address1: SocketAddr,
+    peer_address2: SocketAddr,
+) -> io::Result<()> {
+    let local_address = SocketAddr::new(peer_address1.ip(), 0);
+    let socket = UdpSocket::bind(&mut ctx, local_address)?;
+    let mut socket = socket.connect(peer_address1)?;
+
+    let bytes_written = socket.send(&DATA).await?;
+    assert_eq!(bytes_written, DATA.len());
+
+    let mut socket = socket.connect(peer_address1)?;
+    let bytes_written = socket.send(&DATA).await?;
+    assert_eq!(bytes_written, DATA.len());
+
+    let mut socket = socket.connect(peer_address2)?;
+    let bytes_written = socket.send(&DATA).await?;
+    assert_eq!(bytes_written, DATA.len());
+
+    assert!(socket.take_error().unwrap().is_none());
+
+    Ok(())
+}
+
 /// Bind to any IPv4 port on localhost.
 pub fn any_local_address() -> SocketAddr {
     "127.0.0.1:0".parse().unwrap()
