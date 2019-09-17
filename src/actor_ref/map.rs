f66f5cda1dae1e0b9bc744076e4478af9b41f680
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-09-17 11:35:15 +0200

    Add (Local)TryMap actor references
    Much like the (Local)Map actor reference but uses the TryInto trait.
    Will be used in signal handling in the future.

--- src/actor_ref/map.rs
+++ src/actor_ref/map.rs
@@ -1,5 +1,6 @@
 //! Module containing the mapped actor reference types.
 
+use std::convert::TryFrom;
 use std::{fmt, marker};
 
 use crate::actor_ref::{Send, SendError};
@@ -79,5 +80,83 @@ impl<M> Send for Map<M> {
 impl<M> fmt::Debug for Map<M> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str("MappedActorRef")
+    }
+}
+
+/// Trait to erase the original message type of the actor reference.
+pub(super) trait TryMappedSend<Msg> {
+    fn try_mapped_send(&mut self, msg: Msg) -> Result<(), SendError<Msg>>;
+}
+
+impl<T, M, Msg> TryMappedSend<Msg> for T
+where
+    T: Send<Message = M>,
+    M: TryFrom<Msg, Error = Msg> + Into<Msg>,
+{
+    fn try_mapped_send(&mut self, msg: Msg) -> Result<(), SendError<Msg>> {
+        M::try_from(msg)
+            .map_err(|message| SendError { message })
+            .and_then(|msg| {
+                self.send(msg).map_err(|err| SendError {
+                    message: err.message.into(),
+                })
+            })
+    }
+}
+
+/// Actor reference that attempts to map from one message type to another before
+/// sending.
+///
+/// This reference wraps another actor reference and changes the message type.
+/// This is useful when you need to send to different types of actors from a
+/// central location. See [`ActorRef::local_try_map`].
+///
+/// This actor reference doesn't implement [`Send`] or [`Sync`], see [`TryMap`]
+/// for a version that does.
+///
+/// [`ActorRef::local_try_map`]: crate::actor_ref::ActorRef::local_try_map
+/// [`Send`]: std::marker::Send
+/// [`Sync`]: std::marker::Sync
+pub struct LocalTryMap<M> {
+    pub(super) inner: Box<dyn TryMappedSend<M>>,
+}
+
+impl<M> Send for LocalTryMap<M> {
+    type Message = M;
+
+    fn send(&mut self, msg: Self::Message) -> Result<(), SendError<Self::Message>> {
+        self.inner.try_mapped_send(msg)
+    }
+}
+
+impl<M> fmt::Debug for LocalTryMap<M> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("LocalTryMappedActorRef")
+    }
+}
+
+/// Version of [`LocalTryMap`] that is thread safe, i.e. implements [`Send`] +
+/// [`Sync`].
+///
+/// See [`ActorRef::try_map`].
+///
+/// [`Send`]: std::marker::Send
+/// [`Sync`]: std::marker::Sync
+/// [`ActorRef::try_map`]: crate::actor_ref::ActorRef::try_map
+pub struct TryMap<M> {
+    pub(super) inner: Box<dyn TryMappedSend<M> + marker::Send + Sync>,
+}
+
+impl<M> Send for TryMap<M> {
+    type Message = M;
+
+    fn send(&mut self, msg: Self::Message) -> Result<(), SendError<Self::Message>> {
+        self.inner.try_mapped_send(msg)
+    }
+}
+
+impl<M> fmt::Debug for TryMap<M> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("TryMappedActorRef")
     }
 }
