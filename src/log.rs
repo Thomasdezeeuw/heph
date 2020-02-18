0f3e5aee3fbf0d6c9711f34915568f0fdd9eeece
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-02-18 13:39:39 +0100

    Re-export std-logger::request

--- src/log.rs
+++ src/log.rs
@@ -8,17 +8,17 @@
 //!
 //! The actual logging implementation comes from the [`std-logger`] crate. By
 //! default logs are written to standard error and requests log are written to
-//! standard out. To mark a log message as a request message the
-//! `REQUEST_TARGET` constant can be used, which is re-exported from the
-//! `std-logger` crate.
+//! standard out. To log a request the [`request`] macro can be used, which is
+//! re-exported from the `std-logger` crate.
 //!
 //! To enable logging call [`init`].
 //!
 //! [`std-logger`]: std_logger
+//! [`request`]: crate::log::request
 //! [`init`]: crate::log::init
 
 #[doc(no_inline)]
 pub use log::{debug, error, info, log, log_enabled, trace, warn};
 
 #[doc(no_inline)]
-pub use std_logger::{init, try_init, REQUEST_TARGET};
+pub use std_logger::{init, request, try_init};
