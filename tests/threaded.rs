3cd79bb2ae4a08f5a0ff57d4be3d5a8a26e75072
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-06-13 13:17:20 +0200

    Add some multi-threaded tests

---
+++ tests/threaded.rs
@@ -0,0 +1,116 @@
+//! Tests using multiple threads.
+
+#![feature(track_caller)]
+
+use std::thread;
+use std::time::Duration;
+
+use inbox::{new_small, RecvError, SendError};
+
+#[macro_use]
+mod util;
+
+#[test]
+fn send_single_value() {
+    let (mut sender, mut receiver) = new_small::<usize>();
+
+    start_threads!(
+        {
+            expect_send!(sender, 1);
+        },
+        {
+            expect_recv!(receiver, 1);
+        }
+    );
+}
+
+#[test]
+fn zero_sized_types() {
+    let (mut sender, mut receiver) = new_small();
+
+    start_threads!(
+        {
+            expect_send!(sender, ());
+        },
+        {
+            expect_recv!(receiver, ());
+        }
+    );
+}
+
+#[test]
+fn receive_no_sender() {
+    let (sender, mut receiver) = new_small::<usize>();
+
+    start_threads!(
+        {
+            drop(sender);
+        },
+        {
+            r#loop! {
+                match receiver.try_recv() {
+                    Ok(..) => panic!("unexpected receive of value"),
+                    Err(RecvError::Empty) => {} // Try again.
+                    Err(RecvError::Disconnected) => break,
+                }
+            }
+        }
+    );
+}
+
+#[test]
+fn send_no_receiver() {
+    let (mut sender, receiver) = new_small::<usize>();
+
+    start_threads!(
+        {
+            thread::sleep(Duration::from_millis(1));
+            r#loop! {
+                match sender.try_send(1) {
+                    Ok(()) => {} // Try again.
+                    Err(SendError::Full(..)) => panic!("too slow!"),
+                    Err(SendError::Disconnected(..)) => break,
+                }
+            }
+        },
+        {
+            drop(receiver);
+        }
+    );
+}
+
+#[test]
+fn sender_is_connected() {
+    let (sender, receiver) = new_small::<usize>();
+
+    start_threads!(
+        {
+            r#loop! {
+                if !sender.is_connected() {
+                    break;
+                }
+            }
+        },
+        {
+            drop(receiver);
+        }
+    );
+}
+
+#[test]
+fn receiver_is_connected() {
+    let (sender, receiver) = new_small::<usize>();
+
+    start_threads!(
+        {
+            drop(sender);
+        },
+        {
+            r#loop! {
+                if !receiver.is_connected() {
+                    break;
+                }
+            }
+        }
+    );
+}
