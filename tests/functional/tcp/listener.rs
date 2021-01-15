231dc3eaef815b5ce6afd5d9b27331d948f8ff78
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-01-15 13:51:57 +0100

    Add tests for the TcpListener

---
+++ tests/functional/tcp/listener.rs
@@ -0,0 +1,350 @@
+use std::future::Future;
+use std::io;
+use std::net::SocketAddr;
+use std::pin::Pin;
+use std::task::{self, Poll};
+
+use futures_util::StreamExt;
+
+use heph::actor::{self, Bound};
+use heph::net::{TcpListener, TcpStream};
+use heph::supervisor::NoSupervisor;
+use heph::test::{init_local_actor, poll_actor};
+use heph::{rt, Actor, ActorOptions, ActorRef, Runtime, RuntimeRef};
+
+#[test]
+fn local_addr() {
+    async fn actor(mut ctx: actor::Context<!>) -> Result<(), !> {
+        let address = "127.0.0.1:12345".parse().unwrap();
+        let mut listener = TcpListener::bind(&mut ctx, address).unwrap();
+        assert_eq!(listener.local_addr().unwrap(), address);
+        drop(listener);
+
+        let address = "[::1]:12345".parse().unwrap();
+        let mut listener = TcpListener::bind(&mut ctx, address).unwrap();
+        assert_eq!(listener.local_addr().unwrap(), address);
+
+        Ok(())
+    }
+
+    let actor = actor as fn(_) -> _;
+    let (actor, _) = init_local_actor(actor, ()).unwrap();
+    let mut actor = Box::pin(actor);
+    assert_eq!(poll_actor(Pin::as_mut(&mut actor)), Poll::Ready(Ok(())));
+}
+
+#[test]
+fn local_addr_port_zero() {
+    async fn actor(mut ctx: actor::Context<!>) -> Result<(), !> {
+        let address = "127.0.0.1:0".parse().unwrap();
+        let mut listener = TcpListener::bind(&mut ctx, address).unwrap();
+        let got = listener.local_addr().unwrap();
+        assert_eq!(got.ip(), address.ip());
+        assert!(got.port() != 0);
+        drop(listener);
+
+        let address = "[::1]:0".parse().unwrap();
+        let mut listener = TcpListener::bind(&mut ctx, address).unwrap();
+        let got = listener.local_addr().unwrap();
+        assert_eq!(got.ip(), address.ip());
+        assert!(got.port() != 0);
+
+        Ok(())
+    }
+
+    let actor = actor as fn(_) -> _;
+    let (actor, _) = init_local_actor(actor, ()).unwrap();
+    let mut actor = Box::pin(actor);
+    assert_eq!(poll_actor(Pin::as_mut(&mut actor)), Poll::Ready(Ok(())));
+}
+
+#[test]
+fn ttl() {
+    async fn actor(mut ctx: actor::Context<!>) -> Result<(), !> {
+        let address = "127.0.0.1:0".parse().unwrap();
+        let mut listener = TcpListener::bind(&mut ctx, address).unwrap();
+
+        let initial = listener.ttl().unwrap();
+        let expected = initial + 10;
+        listener.set_ttl(expected).unwrap();
+        assert_eq!(listener.ttl().unwrap(), expected);
+
+        Ok(())
+    }
+
+    let actor = actor as fn(_) -> _;
+    let (actor, _) = init_local_actor(actor, ()).unwrap();
+    let mut actor = Box::pin(actor);
+    assert_eq!(poll_actor(Pin::as_mut(&mut actor)), Poll::Ready(Ok(())));
+}
+
+const DATA: &[u8] = b"Hello world";
+
+async fn stream_actor<K>(mut ctx: actor::Context<SocketAddr, K>) -> Result<(), !>
+where
+    actor::Context<SocketAddr, K>: rt::Access,
+{
+    let address = ctx.receive_next().await.unwrap();
+    let mut stream = TcpStream::connect(&mut ctx, address)
+        .unwrap()
+        .await
+        .unwrap();
+
+    let n = stream.send(DATA).await.unwrap();
+    assert_eq!(n, DATA.len());
+    Ok(())
+}
+
+#[test]
+fn try_accept() {
+    struct YieldOnce(bool);
+
+    impl Future for YieldOnce {
+        type Output = ();
+        fn poll(mut self: Pin<&mut Self>, _: &mut task::Context<'_>) -> Poll<Self::Output> {
+            if self.0 {
+                Poll::Ready(())
+            } else {
+                self.0 = true;
+                Poll::Pending
+            }
+        }
+    }
+
+    const fn yield_once() -> YieldOnce {
+        YieldOnce(false)
+    }
+
+    async fn listener_actor(
+        mut ctx: actor::Context<!>,
+        actor_ref: ActorRef<SocketAddr>,
+    ) -> Result<(), !> {
+        let address = "127.0.0.1:0".parse().unwrap();
+        let mut listener = TcpListener::bind(&mut ctx, address).unwrap();
+
+        let address = listener.local_addr().unwrap();
+        actor_ref.send(address).await.unwrap();
+
+        assert_eq!(
+            listener.try_accept().unwrap_err().kind(),
+            io::ErrorKind::WouldBlock
+        );
+
+        let mut stream = loop {
+            yield_once().await;
+            if let Ok((stream, remote_address)) = listener.try_accept() {
+                assert!(remote_address.ip().is_loopback());
+                break stream.bind_to(&mut ctx).unwrap();
+            }
+        };
+
+        let mut buf = Vec::with_capacity(DATA.len() + 1);
+        let n = stream.recv(&mut buf).await.unwrap();
+        assert_eq!(n, DATA.len());
+        assert_eq!(buf, DATA);
+
+        Ok(())
+    }
+
+    let stream_actor = stream_actor as fn(_) -> _;
+    let (stream_actor, actor_ref) = init_local_actor(stream_actor, ()).unwrap();
+    let stream_actor = Box::pin(stream_actor);
+
+    let listener_actor = listener_actor as fn(_, _) -> _;
+    let (listener_actor, _) = init_local_actor(listener_actor, actor_ref).unwrap();
+    let listener_actor = Box::pin(listener_actor);
+
+    run_both(listener_actor, stream_actor);
+}
+
+#[test]
+fn accept() {
+    async fn listener_actor(
+        mut ctx: actor::Context<!>,
+        actor_ref: ActorRef<SocketAddr>,
+    ) -> Result<(), !> {
+        let address = "127.0.0.1:0".parse().unwrap();
+        let mut listener = TcpListener::bind(&mut ctx, address).unwrap();
+
+        let address = listener.local_addr().unwrap();
+        actor_ref.send(address).await.unwrap();
+
+        let (stream, remote_address) = listener.accept().await.unwrap();
+        let mut stream = stream.bind_to(&mut ctx).unwrap();
+        assert!(remote_address.ip().is_loopback());
+
+        let mut buf = Vec::with_capacity(DATA.len() + 1);
+        let n = stream.recv(&mut buf).await.unwrap();
+        assert_eq!(n, DATA.len());
+        assert_eq!(buf, DATA);
+
+        Ok(())
+    }
+
+    let stream_actor = stream_actor as fn(_) -> _;
+    let (stream_actor, actor_ref) = init_local_actor(stream_actor, ()).unwrap();
+    let stream_actor = Box::pin(stream_actor);
+
+    let listener_actor = listener_actor as fn(_, _) -> _;
+    let (listener_actor, _) = init_local_actor(listener_actor, actor_ref).unwrap();
+    let listener_actor = Box::pin(listener_actor);
+
+    run_both(listener_actor, stream_actor);
+}
+
+#[test]
+fn incoming() {
+    async fn listener_actor(
+        mut ctx: actor::Context<!>,
+        actor_ref: ActorRef<SocketAddr>,
+    ) -> Result<(), !> {
+        let address = "127.0.0.1:0".parse().unwrap();
+        let mut listener = TcpListener::bind(&mut ctx, address).unwrap();
+
+        let address = listener.local_addr().unwrap();
+        actor_ref.send(address).await.unwrap();
+
+        let mut incoming = listener.incoming();
+        let (stream, remote_address) = incoming.next().await.unwrap().unwrap();
+        let mut stream = stream.bind_to(&mut ctx).unwrap();
+        assert!(remote_address.ip().is_loopback());
+
+        let mut buf = Vec::with_capacity(DATA.len() + 1);
+        let n = stream.recv(&mut buf).await.unwrap();
+        assert_eq!(n, DATA.len());
+        assert_eq!(buf, DATA);
+
+        Ok(())
+    }
+
+    let stream_actor = stream_actor as fn(_) -> _;
+    let (stream_actor, actor_ref) = init_local_actor(stream_actor, ()).unwrap();
+    let stream_actor = Box::pin(stream_actor);
+
+    let listener_actor = listener_actor as fn(_, _) -> _;
+    let (listener_actor, _) = init_local_actor(listener_actor, actor_ref).unwrap();
+    let listener_actor = Box::pin(listener_actor);
+
+    run_both(listener_actor, stream_actor);
+}
+
+fn run_both<A1: Actor, A2: Actor>(actor1: Pin<Box<A1>>, actor2: Pin<Box<A2>>) {
+    let mut actor1 = Some(actor1);
+    let mut actor2 = Some(actor2);
+    for _ in 0..10 {
+        if let Some(actor) = actor1.as_mut() {
+            match poll_actor(Pin::as_mut(actor)) {
+                Poll::Pending => {}
+                Poll::Ready(Ok(())) => {
+                    drop(actor);
+                    actor1 = None;
+                }
+                Poll::Ready(Err(_)) => unreachable!(),
+            }
+        }
+
+        if let Some(actor) = actor2.as_mut() {
+            match poll_actor(Pin::as_mut(actor)) {
+                Poll::Pending => {}
+                Poll::Ready(Ok(())) => {
+                    drop(actor);
+                    actor2 = None;
+                }
+                Poll::Ready(Err(_)) => unreachable!(),
+            }
+        } else if actor2.is_none() {
+            // Both `None`.
+            return;
+        }
+    }
+}
+
+#[test]
+fn actor_bound() {
+    async fn listener_actor1<K>(
+        mut ctx: actor::Context<!, K>,
+        actor_ref: ActorRef<TcpListener>,
+    ) -> Result<(), !>
+    where
+        actor::Context<!, K>: rt::Access,
+    {
+        let address = "127.0.0.1:0".parse().unwrap();
+        let listener = TcpListener::bind(&mut ctx, address).unwrap();
+        actor_ref.send(listener).await.unwrap();
+        Ok(())
+    }
+
+    async fn listener_actor2<K>(
+        mut ctx: actor::Context<TcpListener, K>,
+        actor_ref: ActorRef<SocketAddr>,
+    ) -> Result<(), !>
+    where
+        actor::Context<TcpListener, K>: rt::Access,
+    {
+        let mut listener = ctx.receive_next().await.unwrap();
+        listener.bind_to(&mut ctx).unwrap();
+
+        let address = listener.local_addr().unwrap();
+        actor_ref.send(address).await.unwrap();
+
+        let (stream, remote_address) = listener.accept().await.unwrap();
+        let mut stream = stream.bind_to(&mut ctx).unwrap();
+        assert!(remote_address.ip().is_loopback());
+
+        stream.bind_to(&mut ctx).unwrap();
+
+        let mut buf = Vec::with_capacity(DATA.len() + 1);
+        // FIXME: not receiving data.
+        let n = stream.recv(&mut buf).await.unwrap();
+        assert_eq!(n, DATA.len());
+        assert_eq!(buf, DATA);
+        Ok(())
+    }
+
+    fn setup(mut runtime_ref: RuntimeRef) -> Result<(), !> {
+        // Spawn thread-local actors.
+        let stream_ref = runtime_ref.spawn_local(
+            NoSupervisor,
+            stream_actor as fn(_) -> _,
+            (),
+            ActorOptions::default(),
+        );
+        let listener_ref = runtime_ref.spawn_local(
+            NoSupervisor,
+            listener_actor2 as fn(_, _) -> _,
+            stream_ref,
+            ActorOptions::default(),
+        );
+        let _ = runtime_ref.spawn_local(
+            NoSupervisor,
+            listener_actor1 as fn(_, _) -> _,
+            listener_ref,
+            ActorOptions::default(),
+        );
+        Ok(())
+    }
+
+    let mut runtime = Runtime::new().unwrap().with_setup(setup);
+
+    // Spawn thread-safe actors.
+    let stream_ref = runtime.spawn(
+        NoSupervisor,
+        stream_actor as fn(_) -> _,
+        (),
+        ActorOptions::default(),
+    );
+    let listener_ref = runtime.spawn(
+        NoSupervisor,
+        listener_actor2 as fn(_, _) -> _,
+        stream_ref,
+        ActorOptions::default(),
+    );
+    let _ = runtime.spawn(
+        NoSupervisor,
+        listener_actor1 as fn(_, _) -> _,
+        listener_ref,
+        ActorOptions::default(),
+    );
+
+    runtime.start().unwrap();
+}
