e3eb15c6649bdb4c1038b67a20793f7fcd29f59e
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-03-30 16:30:04 +0200

    Deflake issue_145_tcp_server

--- tests/regression/issue_145.rs
+++ tests/regression/issue_145.rs
@@ -3,13 +3,13 @@
 
 use std::io::{self, Read};
 use std::net::SocketAddr;
-use std::sync::{Arc, Barrier, Condvar, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 use heph::actor::messages::Terminate;
 use heph::net::{tcp, TcpListener, TcpServer, TcpStream};
-use heph::rt::{Runtime, ThreadLocal};
+use heph::rt::{Runtime, RuntimeRef, ThreadLocal};
 use heph::supervisor::{NoSupervisor, Supervisor, SupervisorStrategy};
 use heph::{actor, Actor, ActorOptions, ActorRef, NewActor};
 
@@ -20,74 +20,44 @@ fn issue_145_tcp_server() {
     let mut runtime = Runtime::setup().num_threads(N).build().unwrap();
 
     let addresses = Arc::new(Mutex::new(Vec::<SocketAddr>::new()));
+    let servers = Arc::new(Mutex::new(Vec::new()));
+    let addr2 = addresses.clone();
+    let srv2 = servers.clone();
+    let conn_actor = (conn_actor as fn(_, _, _, _, _) -> _)
+        .map_arg(move |(stream, address)| (stream, address, addr2.clone(), srv2.clone()));
     let address = "127.0.0.1:0".parse().unwrap();
-    let addresses2 = addresses.clone();
-    let conn_actor = (conn_actor as fn(_, _, _, _) -> _)
-        .map_arg(move |(stream, address)| (stream, address, addresses2.clone()));
     let server =
         TcpServer::setup(address, NoSupervisor, conn_actor, ActorOptions::default()).unwrap();
     let expected_address = server.local_addr();
 
-    let shared = Arc::new((
-        Mutex::new(()),
-        Condvar::new(),
-        Mutex::new(Option::<ActorRef<_>>::None),
-        Condvar::new(),
-        Barrier::new(2),
-    ));
+    runtime
+        .run_on_workers::<_, !>(move |mut runtime_ref| {
+            let srv_ref = runtime_ref
+                .try_spawn_local(ServerSupervisor, server, (), ActorOptions::default())
+                .unwrap();
+            // NOTE: this is not safe or supported. DO NOT USE THIS.
+            let r = unsafe { std::mem::transmute_copy::<RuntimeRef, usize>(&runtime_ref) };
+            servers.lock().unwrap().push((r, srv_ref));
+            Ok(())
+        })
+        .unwrap();
 
-    // Start a thread that starts a single connection to `expected_address`.
-    let sh = shared.clone();
     let handle = thread::spawn(move || {
-        let (_, setup_cvar, server_ref, _, barrier) = &*sh;
-
         // TODO: replace with a barrier.
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(50));
 
         for _ in 0..N {
-            // Let a setup run.
-            setup_cvar.notify_one();
-
-            // Wait until the setup is complete so the worker thread is actually
-            // running (and able to accept a connection).
-            barrier.wait();
-
             // Create a test connection to check the addresses.
             let mut stream = std::net::TcpStream::connect(&expected_address).unwrap();
             let mut buf = [0; 1];
             let n = stream.read(&mut buf).unwrap();
             assert_eq!(n, 0);
 
-            // Stop the server.
-            server_ref
-                .lock()
-                .unwrap()
-                .take()
-                .unwrap()
-                .try_send(Terminate)
-                .unwrap();
+            // TODO: replace with a barrier.
+            thread::sleep(Duration::from_millis(100));
         }
     });
 
-    runtime
-        .run_on_workers::<_, !>(move |mut runtime_ref| {
-            let (setup_lock, setup_cvar, server_ref, _, barrier) = &*shared;
-            // By grabbing the `setup_lock` we block the other workers, so we
-            // can ensure the thread above send a message the server we start.
-            let setup_lock = setup_lock.lock().unwrap();
-            let _g = setup_cvar.wait(setup_lock).unwrap();
-
-            let srv_ref = runtime_ref
-                .try_spawn_local(ServerSupervisor, server, (), ActorOptions::default())
-                .unwrap();
-            assert!(server_ref.lock().unwrap().replace(srv_ref).is_none());
-
-            // Let the thread start a connection to the server.
-            barrier.wait();
-
-            Ok(())
-        })
-        .unwrap();
     runtime.start().unwrap();
 
     handle.join().unwrap();
@@ -115,13 +85,24 @@ where
 }
 
 async fn conn_actor(
-    _: actor::Context<!, ThreadLocal>,
+    mut ctx: actor::Context<!, ThreadLocal>,
     mut stream: TcpStream,
     _: SocketAddr,
     addresses: Arc<Mutex<Vec<SocketAddr>>>,
+    servers: Arc<Mutex<Vec<(usize, ActorRef<tcp::server::Message>)>>>,
 ) -> Result<(), !> {
     let mut addresses = addresses.lock().unwrap();
     addresses.push(stream.local_addr().unwrap());
+
+    // Shutdown the `TcpServer` that started us to ensure the next request goes
+    // to a different server.
+    // NOTE: this is not safe or supported. DO NOT USE THIS.
+    let r = unsafe { std::mem::transmute_copy::<RuntimeRef, usize>(&*ctx.runtime()) };
+    let mut servers = servers.lock().unwrap();
+    let idx = servers.iter().position(|(rr, _)| r == *rr).unwrap();
+    let (_, server_ref) = servers.remove(idx);
+    server_ref.try_send(Terminate).unwrap();
+
     Ok(())
 }
 
