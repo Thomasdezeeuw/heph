4249526b8b32fb18e3a1a26af36cdff3fd91618c
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-04-30 19:04:16 +0200

    Add StatusCode::phrase
    Returns a static reason phrase, if known.

--- http/src/status_code.rs
+++ http/src/status_code.rs
@@ -68,7 +68,7 @@ impl StatusCode {
     /// IM Used.
     ///
     /// RFC 3229.
-    pub const IM_USED: StatusCode = StatusCode(208);
+    pub const IM_USED: StatusCode = StatusCode(226);
 
     // 3xx range.
     /// Multiple Choices.
@@ -293,6 +293,79 @@ impl StatusCode {
     /// Returns `true` if the status code is in 5xx range.
     pub const fn is_server_error(self) -> bool {
         self.0 >= 500 && self.0 <= 599
+    }
+
+    /// Returns the reason phrase for well known status codes.
+    pub const fn phrase(self) -> Option<&'static str> {
+        match self.0 {
+            100 => Some("Continue"),
+            101 => Some("Switching Protocols"),
+            103 => Some("Processing"),
+            104 => Some("Early Hints"),
+
+            200 => Some("OK"),
+            201 => Some("Created"),
+            202 => Some("Accepted"),
+            203 => Some("Non-Authoritative Information"),
+            204 => Some("No Content"),
+            205 => Some("Reset Content"),
+            206 => Some("Partial Content"),
+            207 => Some("Multi-Status"),
+            208 => Some("Already Reported"),
+            226 => Some("IM Used"),
+
+            300 => Some("Multiple Choices"),
+            301 => Some("Moved Permanently"),
+            302 => Some("Found"),
+            303 => Some("See Other"),
+            304 => Some("Not Modified"),
+            305 => Some("Use Proxy"),
+            307 => Some("Temporary Redirect"),
+            308 => Some("Permanent Redirect"),
+
+            400 => Some("Bad Request"),
+            401 => Some("Unauthorized"),
+            402 => Some("Payment Required"),
+            403 => Some("Forbidden"),
+            404 => Some("Not Found"),
+            405 => Some("Method Not Allowed"),
+            406 => Some("Not Acceptable"),
+            407 => Some("Proxy Authentication Required"),
+            408 => Some("Request Timeout"),
+            409 => Some("Conflict"),
+            410 => Some("Gone"),
+            411 => Some("Length Required"),
+            412 => Some("Precondition Failed"),
+            413 => Some("Payload Too Large"),
+            414 => Some("URI Too Long"),
+            415 => Some("Unsupported Media Type"),
+            416 => Some("Range Not Satisfiable"),
+            417 => Some("Expectation Failed"),
+            421 => Some("Misdirected Request"),
+            422 => Some("Unprocessable Entity"),
+            423 => Some("Locked"),
+            424 => Some("Failed Dependency"),
+            425 => Some("Too Early"),
+            426 => Some("Upgrade Required"),
+            428 => Some("Precondition Required"),
+            429 => Some("Too Many Requests"),
+            431 => Some("Request Header Fields Too Large"),
+            451 => Some("Unavailable For Legal Reasons"),
+
+            500 => Some("Internal Server Error"),
+            501 => Some("Not Implemented"),
+            502 => Some("Bad Gateway"),
+            503 => Some("Service Unavailable"),
+            504 => Some("Gateway Timeout"),
+            505 => Some("HTTP Version Not Supported"),
+            506 => Some("Variant Also Negotiates"),
+            507 => Some("Insufficient Storage"),
+            508 => Some("Loop Detected"),
+            510 => Some("Not Extended"),
+            511 => Some("Network Authentication Required"),
+
+            _ => None,
+        }
     }
 }
 
