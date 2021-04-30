c5189f503cbf3df1e0a61737ed25b8f473d6afbb
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-04-30 12:40:41 +0200

    Implement fmt::{Debug, Display} for ParseIntError

--- http/src/from_bytes.rs
+++ http/src/from_bytes.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 /// Analogous trait to the [`FromStr`] trait.
 ///
 /// [`FromStr`]: std::str::FromStr
@@ -7,7 +9,14 @@ pub trait FromBytes: Sized {
     fn from_bytes(value: &[u8]) -> Result<Self, Self::Err>;
 }
 
+#[derive(Debug)]
 pub struct ParseIntError;
+
+impl fmt::Display for ParseIntError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("invalid integer")
+    }
+}
 
 impl FromBytes for usize {
     type Err = ParseIntError;
