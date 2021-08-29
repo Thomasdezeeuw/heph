55776541fe3a2051803e8b2ea292e8f7538d34df
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-08-29 11:51:14 +0200

    Add metric::MetricsSource
    A wrapper type around Metrics that implements log::kv::Source.

--- src/metrics.rs
+++ src/metrics.rs
@@ -24,6 +24,12 @@ pub trait Collect {
 
     /// Get the current metrics from this type.
     fn metrics(&self) -> &Self::Metrics;
+
+    /// Get the current metrics wrapped in [`MetricsSource`] allowing them to be
+    /// used as [`log::kv::Source`].
+    fn metrics_kv(&self) -> MetricsSource<&Self::Metrics> {
+        MetricsSource(self.metrics())
+    }
 }
 
 /// [Collected] metrics.
@@ -51,6 +57,15 @@ pub enum Metric {
     Counter(usize),
 }
 
+impl Metric {
+    /// Returns `self` as `log::kv::Value`.
+    fn to_kv_value(self) -> log::kv::Value<'static> {
+        match self {
+            Metric::Counter(count) => log::kv::Value::from(count),
+        }
+    }
+}
+
 /// Returns [`Metric::Counter`].
 impl From<Counter> for Metric {
     fn from(counter: Counter) -> Metric {
@@ -71,5 +86,37 @@ impl Counter {
     /// Add `n` to the counter.
     pub(crate) const fn add(&mut self, n: usize) {
         self.0 += n;
+    }
+}
+
+/// Wrapper around metrics `M` to implement [`log::kv::Source`].
+#[derive(Debug, Clone)]
+pub struct MetricsSource<M>(pub M);
+
+impl<M> log::kv::Source for MetricsSource<M>
+where
+    M: Metrics + ?Sized,
+{
+    fn visit<'kvs>(
+        &'kvs self,
+        visitor: &mut dyn log::kv::Visitor<'kvs>,
+    ) -> Result<(), log::kv::Error> {
+        for (name, metric) in self.0.iter() {
+            let key = log::kv::Key::from_str(name);
+            let value = metric.to_kv_value();
+            visitor.visit_pair(key, value)?;
+        }
+        Ok(())
+    }
+
+    fn get<'v>(&'v self, key: log::kv::Key<'_>) -> Option<log::kv::Value<'v>> {
+        let find_key = key.as_str();
+        self.0
+            .iter()
+            .find_map(|(key, value)| (key == find_key).then(|| value.to_kv_value()))
+    }
+
+    fn count(&self) -> usize {
+        self.0.iter().count()
     }
 }
