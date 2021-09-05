c2b8d048df37f81aaa551c3678d11f27143d0ff4
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-09-05 12:21:51 +0200

    Add {RequestHead, ResponseHead}::header
    Convenience method to get the header from the request or response.

--- http/tests/functional/head.rs
+++ http/tests/functional/head.rs
@@ -1,4 +1,6 @@
-use heph_http::head::{RequestHead, ResponseHead};
+use heph_http::head::{
+    Header, HeaderName, Headers, Method, RequestHead, ResponseHead, StatusCode, Version,
+};
 
 use crate::assert_size;
 
@@ -6,4 +8,26 @@ use crate::assert_size;
 fn size() {
     assert_size::<RequestHead>(80);
     assert_size::<ResponseHead>(56);
+}
+
+#[test]
+fn request_header() {
+    let headers = Headers::EMPTY;
+    let mut head = RequestHead::new(Method::Get, "/".to_string(), Version::Http10, headers);
+    assert_eq!(head.header::<&str>(&HeaderName::USER_AGENT), Ok(None));
+
+    let header = Header::new(HeaderName::USER_AGENT, b"Heph-HTTP");
+    head.headers_mut().add(header);
+    assert_eq!(head.header(&HeaderName::USER_AGENT), Ok(Some("Heph-HTTP")));
+}
+
+#[test]
+fn response_header() {
+    let headers = Headers::EMPTY;
+    let mut head = ResponseHead::new(Version::Http10, StatusCode::OK, headers);
+    assert_eq!(head.header::<&str>(&HeaderName::USER_AGENT), Ok(None));
+
+    let header = Header::new(HeaderName::USER_AGENT, b"Heph-HTTP");
+    head.headers_mut().add(header);
+    assert_eq!(head.header(&HeaderName::USER_AGENT), Ok(Some("Heph-HTTP")));
 }
