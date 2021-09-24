4538b5407aa7f2c9be9729fa7516726ba71bafd0
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-09-24 15:11:07 +0200

    Impl PartialEq<str, [u8]> for OneshotBody
    Allows easier comparing in testing.

--- http/tests/functional/body.rs
+++ http/tests/functional/body.rs
@@ -7,3 +7,15 @@ fn oneshot_bytes() {
     let body = OneshotBody::new(BODY1);
     assert_eq!(body.bytes(), BODY1);
 }
+
+#[test]
+fn oneshot_cmp_bytes() {
+    let body = OneshotBody::new(BODY1);
+    assert_eq!(body, BODY1);
+}
+
+#[test]
+fn oneshot_cmp_string() {
+    let body = OneshotBody::new(BODY1);
+    assert_eq!(body, "Hello world!");
+}
