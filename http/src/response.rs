6f00d67d9f13bed58f591b4cf65389ba23ca16b5
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-09-24 16:06:47 +0200

    Add builder pattern to Response
    This add the following methods to Response:
     * ok,
     * created,
     * no_content,
     * moved,
     * found,
     * see_other,
     * not_modified,
     * bad_request,
     * unauthorized,
     * forbidden,
     * not_found,
     * method_not_allowed,
     * gone,
     * length_required,
     * server_error,
     * not_implemented,
     * bad_gateway,
     * unavailable and
     * gateway_timeout.
    All of which return Response<EmptyBody>.
    
    There is also Response::with_body which adds a body to the response.

--- http/src/response.rs
+++ http/src/response.rs
@@ -1,8 +1,9 @@
 use std::fmt;
 use std::ops::{Deref, DerefMut};
 
+use crate::body::EmptyBody;
 use crate::head::ResponseHead;
-use crate::{Headers, StatusCode, Version};
+use crate::{Header, HeaderName, Headers, StatusCode, Version};
 
 /// HTTP response.
 pub struct Response<B> {
@@ -21,7 +22,7 @@ impl<B> Response<B> {
         Response::from_head(ResponseHead::new(version, status, headers), body)
     }
 
-    /// Create a new request from a [`ResponseHead`] and a `body`.
+    /// Create a new response from a [`ResponseHead`] and a `body`.
     pub const fn from_head(head: ResponseHead, body: B) -> Response<B> {
         Response { head, body }
     }
@@ -52,6 +53,142 @@ impl<B> Response<B> {
     // run in constant functions.
     pub fn split(self) -> (ResponseHead, B) {
         (self.head, self.body)
+    }
+}
+
+/// Builder pattern for `Response`.
+///
+/// These methods create a new `Response` using HTTP/1.1, no headers and an
+/// empty body. Use [`Response::with_body`] to add a body to the response.
+impl Response<EmptyBody> {
+    /// Create a 200 OK response.
+    pub const fn ok() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::OK)
+    }
+
+    /// Create a 201 Created response.
+    pub const fn created() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::CREATED)
+    }
+
+    /// Create a 204 No Content response.
+    pub const fn no_content() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::NO_CONTENT)
+    }
+
+    /// Create a 301 Moved Permanently response.
+    ///
+    /// Sets the [Location](HeaderName::LOCATION) header to `location_uri`.
+    pub fn moved(location_uri: &str) -> Response<EmptyBody> {
+        let mut response = Response::build_new(StatusCode::MOVED_PERMANENTLY);
+        response
+            .head
+            .headers_mut()
+            .append(Header::new(HeaderName::LOCATION, location_uri.as_bytes()));
+        response
+    }
+
+    /// Create a 302 Found response.
+    ///
+    /// Sets the [Location](HeaderName::LOCATION) header to `location_uri`.
+    pub fn found(location_uri: &str) -> Response<EmptyBody> {
+        let mut response = Response::build_new(StatusCode::FOUND);
+        response
+            .head
+            .headers_mut()
+            .append(Header::new(HeaderName::LOCATION, location_uri.as_bytes()));
+        response
+    }
+
+    /// Create a 303 See Other response.
+    ///
+    /// Sets the [Location](HeaderName::LOCATION) header to `location_uri`.
+    pub fn see_other(location_uri: &str) -> Response<EmptyBody> {
+        let mut response = Response::build_new(StatusCode::SEE_OTHER);
+        response
+            .head
+            .headers_mut()
+            .append(Header::new(HeaderName::LOCATION, location_uri.as_bytes()));
+        response
+    }
+
+    /// Create a 304 Not Modified response.
+    pub const fn not_modified() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::NOT_MODIFIED)
+    }
+
+    /// Create a 400 Bad Request response.
+    pub const fn bad_request() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::BAD_REQUEST)
+    }
+
+    /// Create a 401 Unauthorized response.
+    pub const fn unauthorized() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::UNAUTHORIZED)
+    }
+
+    /// Create a 403 Forbidden response.
+    pub const fn forbidden() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::FORBIDDEN)
+    }
+
+    /// Create a 404 Not Found response.
+    pub const fn not_found() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::NOT_FOUND)
+    }
+
+    /// Create a 405 Method Not Allow response.
+    pub const fn method_not_allowed() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::METHOD_NOT_ALLOWED)
+        // TODO: set Allow header.
+    }
+
+    /// Create a 410 Gone response.
+    pub const fn gone() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::GONE)
+    }
+
+    /// Create a 411 Length required response.
+    pub const fn length_required() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::LENGTH_REQUIRED)
+    }
+
+    /// Create a 500 Internal Server Error response.
+    pub const fn server_error() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::INTERNAL_SERVER_ERROR)
+    }
+
+    /// Create a 501 Not Implemented response.
+    pub const fn not_implemented() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::NOT_IMPLEMENTED)
+    }
+
+    /// Create a 502 Bad Gateway response.
+    pub const fn bad_gateway() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::BAD_GATEWAY)
+    }
+
+    /// Create a 503 Service Unavailable response.
+    pub const fn unavailable() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::SERVICE_UNAVAILABLE)
+    }
+
+    /// Create a 504 Gateway Timeout response.
+    pub const fn gateway_timeout() -> Response<EmptyBody> {
+        Response::build_new(StatusCode::GATEWAY_TIMEOUT)
+    }
+
+    /// Simple version of [`Response::new`] used by the build functions.
+    const fn build_new(status: StatusCode) -> Response<EmptyBody> {
+        Response::new(Version::Http11, status, Headers::EMPTY, EmptyBody)
+    }
+
+    /// Add a body to the response.
+    pub fn with_body<B>(self, body: B) -> Response<B> {
+        Response {
+            head: self.head,
+            body,
+        }
     }
 }
 
--- http/tests/functional/message.rs
+++ http/tests/functional/message.rs
@@ -87,3 +87,53 @@ fn response_map_body() {
         .map_body(|_body: EmptyBody| OneshotBody::new(BODY1));
     assert_eq!(response.body(), BODY1);
 }
+
+#[test]
+fn response_builder() {
+    let tests: [(fn() -> Response<EmptyBody>, StatusCode); 16] = [
+        (Response::ok, StatusCode::OK),
+        (Response::created, StatusCode::CREATED),
+        (Response::no_content, StatusCode::NO_CONTENT),
+        (Response::not_modified, StatusCode::NOT_MODIFIED),
+        (Response::bad_request, StatusCode::BAD_REQUEST),
+        (Response::unauthorized, StatusCode::UNAUTHORIZED),
+        (Response::forbidden, StatusCode::FORBIDDEN),
+        (Response::not_found, StatusCode::NOT_FOUND),
+        (Response::method_not_allowed, StatusCode::METHOD_NOT_ALLOWED),
+        (Response::gone, StatusCode::GONE),
+        (Response::length_required, StatusCode::LENGTH_REQUIRED),
+        (Response::server_error, StatusCode::INTERNAL_SERVER_ERROR),
+        (Response::not_implemented, StatusCode::NOT_IMPLEMENTED),
+        (Response::bad_gateway, StatusCode::BAD_GATEWAY),
+        (Response::unavailable, StatusCode::SERVICE_UNAVAILABLE),
+        (Response::gateway_timeout, StatusCode::GATEWAY_TIMEOUT),
+    ];
+
+    for (create, expected) in tests {
+        let response = create().with_body::<OneshotBody>(BODY1.into());
+        assert_eq!(response.version(), Version::Http11);
+        assert_eq!(response.status(), expected);
+        assert!(response.headers().is_empty());
+        assert_eq!(response.body(), BODY1);
+    }
+
+    let tests: [(fn(&str) -> Response<EmptyBody>, StatusCode); 3] = [
+        (Response::moved, StatusCode::MOVED_PERMANENTLY),
+        (Response::found, StatusCode::FOUND),
+        (Response::see_other, StatusCode::SEE_OTHER),
+    ];
+
+    for (create, expected) in tests {
+        let uri = "/other_location";
+        let response = create(uri).with_body::<OneshotBody>(BODY1.into());
+        assert_eq!(response.version(), Version::Http11);
+        assert_eq!(response.status(), expected);
+        assert!(!response.headers().is_empty());
+        assert_eq!(response.headers().len(), 1);
+        assert_eq!(
+            response.headers().get_bytes(&HeaderName::LOCATION),
+            Some(uri.as_bytes())
+        );
+        assert_eq!(response.body(), BODY1);
+    }
+}
