6cc1a78c48027dd36eff08774f4cfd2814326aeb
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-11-26 23:12:15 +0100

    Add initial skeleton for the remote crate

---
+++ remote/src/lib.rs
@@ -0,0 +1,3 @@
+#![feature(never_type, generic_associated_types)]
+
+pub mod net_relay;
---
+++ remote/src/net_relay/routers.rs
@@ -0,0 +1,101 @@
+//! Various [`Route`]rs.
+//!
+//! The following routes are provided:
+//!  * [`Relay`] relays all messages to a single actor.
+//!  * [`RelayGroup`] relays all messages to a group of actors.
+//!  * [`Drop`] drops all messages.
+
+use std::fmt;
+use std::future::Future;
+use std::future::{ready, Ready};
+use std::net::SocketAddr;
+
+use heph::actor_ref::{ActorGroup, ActorRef, Delivery, SendError, SendValue};
+
+use crate::net_relay::Route;
+
+impl<F, M, Fut, E> Route<M> for F
+where
+    F: FnMut(M, SocketAddr) -> Fut,
+    Fut: Future<Output = Result<(), E>>,
+    E: fmt::Display,
+{
+    type Route<'a> = Fut;
+    type Error = E;
+
+    fn route<'a>(&'a mut self, msg: M, source: SocketAddr) -> Self::Route<'a> {
+        (self)(msg, source)
+    }
+}
+
+/// [`Route`] implementation that routes all messages to a single
+/// actor.
+#[derive(Debug)]
+pub struct Relay<M> {
+    actor_ref: ActorRef<M>,
+}
+
+impl<M> Relay<M> {
+    /// Relay all remote messages to the `actor_ref`.
+    pub const fn to(actor_ref: ActorRef<M>) -> Relay<M> {
+        Relay { actor_ref }
+    }
+}
+
+impl<Msg, M> Route<Msg> for Relay<M>
+where
+    Msg: Into<M>,
+    M: 'static + Unpin,
+{
+    type Error = SendError;
+    type Route<'a> = SendValue<'a, M>;
+
+    fn route<'a>(&'a mut self, msg: Msg, _: SocketAddr) -> Self::Route<'a> {
+        self.actor_ref.send(msg)
+    }
+}
+
+/// [`Route`] implementation that routes all messages to a group of
+/// actors.
+#[derive(Debug)]
+pub struct RelayGroup<M> {
+    actor_group: ActorGroup<M>,
+    delivery: Delivery,
+}
+
+impl<M> RelayGroup<M> {
+    /// Relay all remote messages to the `actor_group`.
+    pub const fn to(actor_group: ActorGroup<M>, delivery: Delivery) -> RelayGroup<M> {
+        RelayGroup {
+            actor_group,
+            delivery,
+        }
+    }
+}
+
+impl<Msg, M> Route<Msg> for RelayGroup<M>
+where
+    Msg: Into<M> + Clone,
+    M: 'static + Unpin,
+{
+    type Error = !;
+    type Route<'a> = Ready<Result<(), Self::Error>>;
+
+    fn route<'a>(&'a mut self, msg: Msg, _: SocketAddr) -> Self::Route<'a> {
+        let _ = self.actor_group.try_send(msg, self.delivery);
+        ready(Ok(()))
+    }
+}
+
+/// Router that drops all messages.
+#[derive(Debug)]
+pub struct Drop;
+
+impl<M> Route<M> for Drop {
+    type Error = !;
+    type Route<'a> = Ready<Result<(), Self::Error>>;
+
+    fn route<'a>(&'a mut self, _: M, _: SocketAddr) -> Self::Route<'a> {
+        ready(Ok(()))
+    }
+}
