a2a0578c5ad011126be386cb232e2ae2b227e03c
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-11-27 00:59:56 +0100

    Implement NewActor for net relay Config
    For the UDP variant.
    
    This adds the S, Out, In and RT generic parameters to Config as they are
    needed in the NewActor implementation. Furthermore this makes
    UdpRelayMessage (previously udp::RelayMessage) public as it's also
    exposed in the implementation.

--- remote/src/net_relay/udp.rs
+++ remote/src/net_relay/udp.rs
@@ -17,28 +17,55 @@ use crate::net_relay::{Route, Serde};
 
 const MAX_PACKET_SIZE: usize = 1 << 16; // ~65kb.
 
-/// Message type used for network relays.
-pub(crate) enum RelayMessage<M> {
+/// Message type used for network relays using UDP.
+///
+/// # Notes
+///
+/// When using an [`ActorRef`] with this type you can use [`ActorRef::map_fn`]
+/// to e.g. always add an address and change the message type to something more
+/// convenient.
+///
+/// ```
+/// # #![allow(dead_code)]
+/// use heph::ActorRef;
+/// use heph_remote::net_relay::UdpRelayMessage;
+///
+/// # return;
+/// // Original actor reference from spawing the relay actor.
+/// let actor_ref: ActorRef<UdpRelayMessage<String>> = // ...
+/// # todo!();
+///
+/// // A remote target we want to send our messages to.
+/// let target = "127.0.0.1:8080".parse().unwrap();
+/// // Using the `map_fn` we can always set the target and use the `String` type
+/// // as message.
+/// let actor_ref: ActorRef<String> = actor_ref
+///     .map_fn(move |message| UdpRelayMessage::Relay { message, target });
+/// ```
+///
+/// [`ActorRef`]: heph::ActorRef
+/// [`ActorRef::map_fn`]: heph::ActorRef::map_fn
+pub enum UdpRelayMessage<M> {
     /// Relay message `M` to `target`.
     Relay { message: M, target: SocketAddr },
     /// Stop the relay.
     Terminate,
 }
 
-impl<M> From<Terminate> for RelayMessage<M> {
-    fn from(_: Terminate) -> RelayMessage<M> {
-        RelayMessage::Terminate
+impl<M> From<Terminate> for UdpRelayMessage<M> {
+    fn from(_: Terminate) -> UdpRelayMessage<M> {
+        UdpRelayMessage::Terminate
     }
 }
 
-impl<M> TryFrom<Signal> for RelayMessage<M> {
+impl<M> TryFrom<Signal> for UdpRelayMessage<M> {
     type Error = ();
 
     /// Converts [`Signal::Interrupt`], [`Signal::Terminate`] and
     /// [`Signal::Quit`], fails for all other signals (by returning `Err(())`).
     fn try_from(signal: Signal) -> Result<Self, Self::Error> {
         match signal {
-            Signal::Interrupt | Signal::Terminate | Signal::Quit => Ok(RelayMessage::Terminate),
+            Signal::Interrupt | Signal::Terminate | Signal::Quit => Ok(UdpRelayMessage::Terminate),
             _ => Err(()),
         }
     }
@@ -50,7 +77,7 @@ impl<M> TryFrom<Signal> for RelayMessage<M> {
 /// actor using UDP. Any `In`coming message on the same socket will be routed
 /// using the `R`outer.
 pub(crate) async fn remote_relay<S, Out, In, R, RT>(
-    mut ctx: actor::Context<RelayMessage<Out>, RT>,
+    mut ctx: actor::Context<UdpRelayMessage<Out>, RT>,
     local_address: SocketAddr,
     mut router: R,
 ) -> io::Result<()>
@@ -70,10 +97,10 @@ where
             Ok(Ok(msg)) => match msg {
                 // Received an outgoing message we want to relay to a remote
                 // actor.
-                RelayMessage::Relay { message, target } => {
+                UdpRelayMessage::Relay { message, target } => {
                     send_message::<S, Out>(&mut socket, &mut buf, target, &message).await?
                 }
-                RelayMessage::Terminate => return Ok(()),
+                UdpRelayMessage::Terminate => return Ok(()),
             },
             // TODO: do we want to continue here? Still relaying messages from
             // remote actors.
