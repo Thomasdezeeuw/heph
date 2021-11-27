dfd4aae76c7f729dfa0953c91000ab60c5ff36e4
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-11-27 03:57:08 +0100

    Add UUID to all send message in net relay
    To be used in RPC.

--- remote/src/net_relay/udp.rs
+++ remote/src/net_relay/udp.rs
@@ -1,8 +1,9 @@
 //! Module with the UDP implementation of the net relay.
 
 use std::convert::TryFrom;
-use std::io;
+use std::marker::PhantomData;
 use std::net::SocketAddr;
+use std::{fmt, io};
 
 use heph::actor::messages::Terminate;
 use heph::actor::{self, NoMessages};
@@ -10,9 +11,10 @@ use heph::net::UdpSocket;
 use heph::rt::{self, Signal};
 use heph::util::either;
 use log::warn;
-use serde::de::DeserializeOwned;
-use serde::Serialize;
+use serde::de::{self, Deserialize, DeserializeOwned, Deserializer, MapAccess, Visitor};
+use serde::ser::{Serialize, SerializeStruct, Serializer};
 
+use crate::net_relay::uuid::{Uuid, UuidGenerator};
 use crate::net_relay::{Route, Serde};
 
 const MAX_PACKET_SIZE: usize = 1 << 16; // ~65kb.
@@ -91,6 +93,7 @@ where
 {
     let mut socket = UdpSocket::bind(&mut ctx, local_address)?;
     let mut buf = Vec::with_capacity(MAX_PACKET_SIZE);
+    let mut uuid_gen = UuidGenerator::new();
 
     loop {
         buf.clear();
@@ -99,7 +102,8 @@ where
                 // Received an outgoing message we want to relay to a remote
                 // actor.
                 UdpRelayMessage::Relay { message, target } => {
-                    send_message::<S, Out>(&mut socket, &mut buf, target, &message).await?
+                    send_message::<S, Out>(&mut socket, &mut buf, &mut uuid_gen, target, &message)
+                        .await?
                 }
                 UdpRelayMessage::Terminate => return Ok(()),
             },
@@ -115,10 +119,122 @@ where
     }
 }
 
+/// Message type that wraps the user's message `M`.
+struct Message<M> {
+    uuid: Uuid,
+    msg: M,
+}
+
+// NOTE: manually implementing this instead of deriving to not pull in a bunch
+// of dependencies.
+impl<'de, M> Deserialize<'de> for Message<M>
+where
+    M: Deserialize<'de>,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        enum Field {
+            Uuid,
+            Msg,
+        }
+
+        impl<'de> Deserialize<'de> for Field {
+            fn deserialize<D>(deserializer: D) -> Result<Field, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                struct FieldVisitor;
+
+                impl<'de> Visitor<'de> for FieldVisitor {
+                    type Value = Field;
+
+                    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                        formatter.write_str("`uuid` or `message`")
+                    }
+
+                    fn visit_str<E>(self, value: &str) -> Result<Field, E>
+                    where
+                        E: de::Error,
+                    {
+                        match value {
+                            "uuid" => Ok(Field::Uuid),
+                            "message" => Ok(Field::Msg),
+                            _ => Err(de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+
+                deserializer.deserialize_identifier(FieldVisitor)
+            }
+        }
+
+        struct MessageVisitor<M>(PhantomData<M>);
+
+        impl<'de, M> Visitor<'de> for MessageVisitor<M>
+        where
+            M: Deserialize<'de>,
+        {
+            type Value = Message<M>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("struct Message")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> Result<Message<M>, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                let mut uuid = None;
+                let mut msg = None;
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        Field::Uuid => {
+                            if uuid.is_some() {
+                                return Err(de::Error::duplicate_field("uuid"));
+                            }
+                            uuid = Some(map.next_value()?);
+                        }
+                        Field::Msg => {
+                            if msg.is_some() {
+                                return Err(de::Error::duplicate_field("message"));
+                            }
+                            msg = Some(map.next_value()?);
+                        }
+                    }
+                }
+                let uuid = uuid.ok_or_else(|| de::Error::missing_field("uuid"))?;
+                let msg = msg.ok_or_else(|| de::Error::missing_field("message"))?;
+                Ok(Message { uuid, msg })
+            }
+        }
+
+        const FIELDS: &'static [&'static str] = &["uuid", "message"];
+        deserializer.deserialize_struct("Message", FIELDS, MessageVisitor(PhantomData))
+    }
+}
+
+impl<M> Serialize for Message<M>
+where
+    M: Serialize,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("Message", 2)?;
+        state.serialize_field("uuid", &self.uuid)?;
+        state.serialize_field("message", &self.msg)?;
+        state.end()
+    }
+}
+
 /// Send a `msg` to a remote actor at `target` address, using `socket`.
 async fn send_message<S, M>(
     socket: &mut UdpSocket,
     buf: &mut Vec<u8>,
+    uuid_gen: &mut UuidGenerator,
     target: SocketAddr,
     msg: &M,
 ) -> io::Result<()>
@@ -127,7 +243,9 @@ where
     M: Serialize,
 {
     // Serialise the message to our buffer first.
-    if let Err(err) = S::to_buf(&mut *buf, msg) {
+    let uuid = uuid_gen.next();
+    let msg = Message { uuid, msg };
+    if let Err(err) = S::to_buf(&mut *buf, &msg) {
         warn!("error serialising message (for {}): {}", target, err);
         // Don't want to stop the actor for this.
         return Ok(());
@@ -163,8 +281,8 @@ where
     R: Route<M>,
     M: DeserializeOwned,
 {
-    match S::from_slice(buf) {
-        Ok(msg) => match router.route(msg, source).await {
+    match S::from_slice::<Message<M>>(buf) {
+        Ok(msg) => match router.route(msg.msg, source).await {
             Ok(()) => Ok(()),
             Err(err) => {
                 let msg = format!("failed to route message (from {}): {}", source, err);
---
+++ remote/src/net_relay/uuid.rs
@@ -0,0 +1,195 @@
+use std::convert::{TryFrom, TryInto};
+use std::fmt;
+use std::ops::Range;
+
+use getrandom::getrandom;
+use log::warn;
+use serde::de::{self, Deserialize, Deserializer, Unexpected, Visitor};
+use serde::{Serialize, Serializer};
+
+/// Quick and dirty UUID generator.
+///
+/// # Notes
+///
+/// This does not create random UUIDs and following UUIDs can be easily guessed
+/// based on the previous UUIDs.
+pub(crate) struct UuidGenerator(u64, u64);
+
+impl UuidGenerator {
+    /// Create a new generator.
+    pub(crate) fn new() -> UuidGenerator {
+        let mut bytes = [0u8; 16];
+        match getrandom(&mut bytes) {
+            Ok(_) => {
+                let b0 = u64::from_be_bytes(TryInto::try_into(&bytes[0..8]).unwrap());
+                let b1 = u64::from_be_bytes(TryInto::try_into(&bytes[8..16]).unwrap());
+                UuidGenerator(b0, b1)
+            }
+            Err(err) => {
+                warn!("unable to get random bytes, using a fallback: {}", err);
+                UuidGenerator(9396178701149223067, 6169990013871724815)
+            }
+        }
+    }
+
+    /// Generate the next UUID.
+    pub(crate) fn next(&mut self) -> Uuid {
+        let bytes1 = self.0;
+        let bytes0 = self.1;
+        self.0 = self.0.wrapping_add(self.1);
+        self.1 = self.1.wrapping_add(92478483931537517);
+        let mut bytes = ((bytes0 as u128) + ((bytes1 as u128) << 64)).to_ne_bytes();
+
+        // Set the variant to RFC4122 (section 4.1.1).
+        bytes[8] = (bytes[8] & 0x3f) | 0x80;
+        // Set the version to 4 (random) (section 4.1.3).
+        bytes[6] = (bytes[6] & 0x0f) | (4 << 4);
+
+        Uuid(bytes)
+    }
+}
+
+/// Universally Unique(-ish) Identifier (UUID).
+pub(crate) struct Uuid([u8; 16]);
+
+/// Groups of 8, 4, 4, 4, 12 bytes.
+const HYPHENS: [Range<usize>; 5] = [0..8, 9..13, 14..18, 19..23, 24..36];
+/// 16 characters used to represents bytes in hexadecimal.
+const HEX_CHARS: [u8; 16] = [
+    b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'a', b'b', b'c', b'd', b'e', b'f',
+];
+/// Positions of the bytes for each grouping.
+const BYTE_POSITIONS: [usize; 6] = [0, 4, 6, 8, 10, 16];
+/// Positions of the hypens (`-`).
+const HYPHEN_POSITIONS: [usize; 4] = [8, 13, 18, 23];
+
+// NOTE: manually implementing this instead of deriving to not pull in a bunch
+// of dependencies.
+impl<'de> Deserialize<'de> for Uuid {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        if deserializer.is_human_readable() {
+            struct StrVisitor;
+
+            impl<'de> Visitor<'de> for StrVisitor {
+                type Value = Uuid;
+
+                fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                    formatter.write_str("UUID string")
+                }
+
+                fn visit_str<E>(self, input: &str) -> Result<Self::Value, E>
+                where
+                    E: de::Error,
+                {
+                    let res = match input.len() {
+                        32 => from_hex(input.as_bytes()),
+                        36 => from_hex_hyphenated(input.as_bytes()),
+                        n => return Err(de::Error::invalid_length(n, &"32 or 36 bytes")),
+                    };
+                    match res {
+                        Ok(uuid) => Ok(uuid),
+                        Err(()) => Err(de::Error::invalid_value(
+                            Unexpected::Str(input),
+                            &"UUID string",
+                        )),
+                    }
+                }
+            }
+
+            deserializer.deserialize_str(StrVisitor)
+        } else {
+            struct BytesVisitor;
+
+            impl<'de> Visitor<'de> for BytesVisitor {
+                type Value = Uuid;
+
+                fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                    formatter.write_str("UUID bytes")
+                }
+
+                fn visit_bytes<E>(self, bytes: &[u8]) -> Result<Self::Value, E>
+                where
+                    E: de::Error,
+                {
+                    match TryFrom::try_from(bytes) {
+                        Ok(bytes) => Ok(Uuid(bytes)),
+                        Err(_) => Err(de::Error::invalid_length(bytes.len(), &"32 bytes")),
+                    }
+                }
+            }
+
+            deserializer.deserialize_str(BytesVisitor)
+        }
+    }
+}
+
+/// `input` must be 32 bytes long.
+fn from_hex(input: &[u8]) -> Result<Uuid, ()> {
+    let _ = input[31];
+    let mut bytes = [0; 16];
+    for (idx, chunk) in input.chunks_exact(2).enumerate() {
+        let lower = from_hex_byte(chunk[1])?;
+        let higher = from_hex_byte(chunk[0])?;
+        bytes[idx] = lower | (higher << 4);
+    }
+    Ok(Uuid(bytes))
+}
+
+/// `input` must be 36 bytes long.
+fn from_hex_hyphenated(input: &[u8]) -> Result<Uuid, ()> {
+    let _ = input[35];
+    let mut bytes = [0; 16];
+    let mut idx = 0;
+    for group in HYPHENS {
+        let group_end = group.end;
+        for chunk in input[group].chunks_exact(2) {
+            let lower = from_hex_byte(chunk[1])?;
+            let higher = from_hex_byte(chunk[0])?;
+            bytes[idx] = lower | (higher << 4);
+            idx += 1;
+        }
+
+        if let Some(b) = input.get(group_end) {
+            if *b != b'-' {
+                return Err(());
+            }
+        }
+    }
+    Ok(Uuid(bytes))
+}
+
+fn from_hex_byte(b: u8) -> Result<u8, ()> {
+    match b {
+        b'A'..=b'F' => Ok(b - b'A' + 10),
+        b'a'..=b'f' => Ok(b - b'a' + 10),
+        b'0'..=b'9' => Ok(b - b'0'),
+        _ => Err(()),
+    }
+}
+
+impl Serialize for Uuid {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        if serializer.is_human_readable() {
+            let mut buf = [0; 36];
+            for group in 0..5 {
+                for idx in BYTE_POSITIONS[group]..BYTE_POSITIONS[group + 1] {
+                    let byte = self.0[idx];
+                    let out_idx = group + 2 * idx;
+                    buf[out_idx] = HEX_CHARS[(byte >> 4) as usize];
+                    buf[out_idx + 1] = HEX_CHARS[(byte & 0b1111) as usize];
+                }
+                if group != 4 {
+                    buf[HYPHEN_POSITIONS[group]] = b'-';
+                }
+            }
+            // SAFETY: All bytes are one of `HEX_CHARS`, which are all valid
+            // UTF-8 characters.
+            serializer.serialize_str(unsafe { std::str::from_utf8_unchecked(&buf) })
+        } else {
+            serializer.serialize_bytes(&self.0)
+        }
+    }
+}
