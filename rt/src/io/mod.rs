d0ca2ff8e172a0a6df246926e954a0b1a670ea7f
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2023-03-18 22:34:27 +0100

    Add wrappers around A10's I/O Futures

--- rt/src/io/mod.rs
+++ rt/src/io/mod.rs
@@ -4,9 +4,16 @@
 //! define the requirements on buffers in use in I/O. Additionally the
 //! [`BufSlice`] and [`BufMutSlice`] traits define the behaviour of buffers in
 //! vectored I/O.
+//!
+//! Finally this module contains a number of [`Future`] implementation that
+//! facilitate I/O operations.
 
 // For ease of use within the crate.
 pub(crate) use std::io::{Error, Result};
 
 mod buf;
+pub(crate) use buf::BufWrapper;
 pub use buf::{Buf, BufMut, BufMutSlice, BufSlice};
+
+mod futures;
+pub use futures::{Read, ReadVectored, Write, WriteVectored};
