a60fe08cb62bc19eb5e51ad42b71c795bf003e6a
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2023-04-09 14:22:52 +0200

    Make net::SockAddr private
    Instead of using the public in private hack, now that the net Future
    types are also private.

--- rt/src/net/mod.rs
+++ rt/src/net/mod.rs
@@ -44,8 +44,9 @@
 //! [bound]: crate::Bound
 //! [`Bound`]: crate::Bound
 
-use std::io;
-use std::net::SocketAddr;
+use std::mem::{size_of, MaybeUninit};
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
+use std::{fmt, io, ptr};
 
 mod futures;
 pub mod tcp;
@@ -85,116 +86,104 @@ fn convert_address(address: socket2::SockAddr) -> io::Result<SocketAddr> {
     }
 }
 
-mod address {
-    //! Public in private hack.
+// TODO: merge this into socket2 in some form.
+#[derive(Copy, Clone)]
+pub(crate) union SockAddr {
+    ip: libc::sockaddr,
+    ipv4: libc::sockaddr_in,
+    ipv6: libc::sockaddr_in6,
+}
 
-    use std::mem::{size_of, MaybeUninit};
-    use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
-    use std::{fmt, ptr};
-
-    // TODO: merge this into socket2 in some form.
-    #[derive(Copy, Clone)]
-    pub union SockAddr {
-        ip: libc::sockaddr,
-        ipv4: libc::sockaddr_in,
-        ipv6: libc::sockaddr_in6,
-    }
-
-    impl From<SocketAddr> for SockAddr {
-        fn from(addr: SocketAddr) -> SockAddr {
-            match addr {
-                SocketAddr::V4(addr) => addr.into(),
-                SocketAddr::V6(addr) => addr.into(),
-            }
-        }
-    }
-
-    impl From<SocketAddrV4> for SockAddr {
-        fn from(addr: SocketAddrV4) -> SockAddr {
-            SockAddr {
-                ipv4: libc::sockaddr_in {
-                    sin_family: libc::AF_INET as libc::sa_family_t,
-                    sin_port: addr.port().to_be(),
-                    sin_addr: libc::in_addr {
-                        s_addr: u32::from_ne_bytes(addr.ip().octets()),
-                    },
-                    sin_zero: Default::default(),
-                },
-            }
-        }
-    }
-
-    impl From<SocketAddrV6> for SockAddr {
-        fn from(addr: SocketAddrV6) -> SockAddr {
-            SockAddr {
-                ipv6: libc::sockaddr_in6 {
-                    sin6_family: libc::AF_INET6 as libc::sa_family_t,
-                    sin6_port: addr.port().to_be(),
-                    sin6_addr: libc::in6_addr {
-                        s6_addr: addr.ip().octets(),
-                    },
-                    sin6_flowinfo: addr.flowinfo(),
-                    sin6_scope_id: addr.scope_id(),
-                },
-            }
-        }
-    }
-
-    impl From<SockAddr> for SocketAddr {
-        fn from(addr: SockAddr) -> SocketAddr {
-            match unsafe { addr.ip.sa_family as _ } {
-                libc::AF_INET => {
-                    let addr = unsafe { addr.ipv4 };
-                    let ip = Ipv4Addr::from(addr.sin_addr.s_addr.to_ne_bytes());
-                    let port = u16::from_be(addr.sin_port);
-                    SocketAddr::V4(SocketAddrV4::new(ip, port))
-                }
-                libc::AF_INET6 => {
-                    let addr = unsafe { addr.ipv6 };
-                    let ip = Ipv6Addr::from(addr.sin6_addr.s6_addr);
-                    let port = u16::from_be(addr.sin6_port);
-                    SocketAddr::V6(SocketAddrV6::new(
-                        ip,
-                        port,
-                        addr.sin6_flowinfo,
-                        addr.sin6_scope_id,
-                    ))
-                }
-                _ => unreachable!(),
-            }
-        }
-    }
-
-    impl a10::net::SocketAddress for SockAddr {
-        unsafe fn as_ptr(&self) -> (*const libc::sockaddr, libc::socklen_t) {
-            match unsafe { self.ip.sa_family as _ } {
-                libc::AF_INET => self.ipv4.as_ptr(),
-                libc::AF_INET6 => self.ipv6.as_ptr(),
-                _ => unreachable!(),
-            }
-        }
-
-        unsafe fn as_mut_ptr(
-            this: &mut MaybeUninit<Self>,
-        ) -> (*mut libc::sockaddr, libc::socklen_t) {
-            (
-                ptr::addr_of_mut!(*this.as_mut_ptr()).cast(),
-                size_of::<SockAddr>() as _,
-            )
-        }
-
-        unsafe fn init(this: MaybeUninit<Self>, length: libc::socklen_t) -> Self {
-            debug_assert!(length >= size_of::<libc::sa_family_t>() as _);
-            // SAFETY: caller must initialise the address.
-            this.assume_init()
-        }
-    }
-
-    impl fmt::Debug for SockAddr {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            SocketAddr::from(*self).fmt(f)
+impl From<SocketAddr> for SockAddr {
+    fn from(addr: SocketAddr) -> SockAddr {
+        match addr {
+            SocketAddr::V4(addr) => addr.into(),
+            SocketAddr::V6(addr) => addr.into(),
         }
     }
 }
 
-use address::SockAddr;
+impl From<SocketAddrV4> for SockAddr {
+    fn from(addr: SocketAddrV4) -> SockAddr {
+        SockAddr {
+            ipv4: libc::sockaddr_in {
+                sin_family: libc::AF_INET as libc::sa_family_t,
+                sin_port: addr.port().to_be(),
+                sin_addr: libc::in_addr {
+                    s_addr: u32::from_ne_bytes(addr.ip().octets()),
+                },
+                sin_zero: Default::default(),
+            },
+        }
+    }
+}
+
+impl From<SocketAddrV6> for SockAddr {
+    fn from(addr: SocketAddrV6) -> SockAddr {
+        SockAddr {
+            ipv6: libc::sockaddr_in6 {
+                sin6_family: libc::AF_INET6 as libc::sa_family_t,
+                sin6_port: addr.port().to_be(),
+                sin6_addr: libc::in6_addr {
+                    s6_addr: addr.ip().octets(),
+                },
+                sin6_flowinfo: addr.flowinfo(),
+                sin6_scope_id: addr.scope_id(),
+            },
+        }
+    }
+}
+
+impl From<SockAddr> for SocketAddr {
+    fn from(addr: SockAddr) -> SocketAddr {
+        match unsafe { addr.ip.sa_family as _ } {
+            libc::AF_INET => {
+                let addr = unsafe { addr.ipv4 };
+                let ip = Ipv4Addr::from(addr.sin_addr.s_addr.to_ne_bytes());
+                let port = u16::from_be(addr.sin_port);
+                SocketAddr::V4(SocketAddrV4::new(ip, port))
+            }
+            libc::AF_INET6 => {
+                let addr = unsafe { addr.ipv6 };
+                let ip = Ipv6Addr::from(addr.sin6_addr.s6_addr);
+                let port = u16::from_be(addr.sin6_port);
+                SocketAddr::V6(SocketAddrV6::new(
+                    ip,
+                    port,
+                    addr.sin6_flowinfo,
+                    addr.sin6_scope_id,
+                ))
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl a10::net::SocketAddress for SockAddr {
+    unsafe fn as_ptr(&self) -> (*const libc::sockaddr, libc::socklen_t) {
+        match unsafe { self.ip.sa_family as _ } {
+            libc::AF_INET => self.ipv4.as_ptr(),
+            libc::AF_INET6 => self.ipv6.as_ptr(),
+            _ => unreachable!(),
+        }
+    }
+
+    unsafe fn as_mut_ptr(this: &mut MaybeUninit<Self>) -> (*mut libc::sockaddr, libc::socklen_t) {
+        (
+            ptr::addr_of_mut!(*this.as_mut_ptr()).cast(),
+            size_of::<SockAddr>() as _,
+        )
+    }
+
+    unsafe fn init(this: MaybeUninit<Self>, length: libc::socklen_t) -> Self {
+        debug_assert!(length >= size_of::<libc::sa_family_t>() as _);
+        // SAFETY: caller must initialise the address.
+        this.assume_init()
+    }
+}
+
+impl fmt::Debug for SockAddr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        SocketAddr::from(*self).fmt(f)
+    }
+}
