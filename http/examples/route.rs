4b4853dd5d0c4c1eb3ce7dbb7b896fb76f89cffa
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2023-05-25 16:51:41 +0200

    Fix Heph-HTTP examples

--- http/examples/route.rs
+++ http/examples/route.rs
@@ -1,31 +1,30 @@
 #![feature(never_type)]
 
 use std::io;
-use std::net::SocketAddr;
 use std::time::Duration;
 
-use heph::actor::{self, Actor, NewActor};
-use heph::net::TcpStream;
-use heph::rt::{self, Runtime, ThreadLocal};
-use heph::supervisor::{Supervisor, SupervisorStrategy};
-use heph::timer::Deadline;
+use heph::actor::{self};
+use heph::supervisor::SupervisorStrategy;
 use heph_http::body::OneshotBody;
-use heph_http::{self as http, route, HttpServer, Request, Response};
+use heph_http::{self as http, route, server, Request, Response};
+use heph_rt::net::TcpStream;
 use heph_rt::spawn::options::{ActorOptions, Priority};
+use heph_rt::timer::Deadline;
+use heph_rt::{Runtime, ThreadLocal};
 use log::{error, info, warn};
 
-fn main() -> Result<(), rt::Error> {
+fn main() -> Result<(), heph_rt::Error> {
     std_logger::Config::logfmt().init();
 
-    let actor = http_actor as fn(_, _, _) -> _;
+    let actor = http_actor as fn(_, _) -> _;
     let address = "127.0.0.1:7890".parse().unwrap();
-    let server = HttpServer::setup(address, conn_supervisor, actor, ActorOptions::default())
-        .map_err(rt::Error::setup)?;
+    let server = server::setup(address, conn_supervisor, actor, ActorOptions::default())
+        .map_err(heph_rt::Error::setup)?;
 
     let mut runtime = Runtime::setup().use_all_cores().build()?;
     runtime.run_on_workers(move |mut runtime_ref| -> io::Result<()> {
         let options = ActorOptions::default().with_priority(Priority::LOW);
-        let server_ref = runtime_ref.try_spawn_local(ServerSupervisor, server, (), options)?;
+        let server_ref = runtime_ref.spawn_local(server_supervisor, server, (), options);
 
         runtime_ref.receive_signals(server_ref.try_map());
         Ok(())
@@ -34,37 +33,20 @@ fn main() -> Result<(), rt::Error> {
     runtime.start()
 }
 
-/// Our supervisor for the TCP server.
-#[derive(Copy, Clone, Debug)]
-struct ServerSupervisor;
-
-impl<NA> Supervisor<NA> for ServerSupervisor
-where
-    NA: NewActor<Argument = (), Error = io::Error>,
-    NA::Actor: Actor<Error = http::server::Error<!>>,
-{
-    fn decide(&mut self, err: http::server::Error<!>) -> SupervisorStrategy<()> {
-        use http::server::Error::*;
-        match err {
-            Accept(err) => {
-                error!("error accepting new connection: {err}");
-                SupervisorStrategy::Restart(())
-            }
-            NewActor(_) => unreachable!(),
+fn server_supervisor(err: server::Error<!>) -> SupervisorStrategy<()> {
+    match err {
+        // When we hit an error accepting a connection we'll drop the old
+        // server and create a new one.
+        server::Error::Accept(err) => {
+            error!("error accepting new connection: {err}");
+            SupervisorStrategy::Restart(())
         }
-    }
-
-    fn decide_on_restart_error(&mut self, err: io::Error) -> SupervisorStrategy<()> {
-        error!("error restarting the TCP server: {err}");
-        SupervisorStrategy::Stop
-    }
-
-    fn second_restart_error(&mut self, err: io::Error) {
-        error!("error restarting the actor a second time: {err}");
+        // Async function never return an error creating a new actor.
+        server::Error::NewActor(_) => unreachable!(),
     }
 }
 
-fn conn_supervisor(err: io::Error) -> SupervisorStrategy<(TcpStream, SocketAddr)> {
+fn conn_supervisor(err: io::Error) -> SupervisorStrategy<TcpStream> {
     error!("error handling connection: {err}");
     SupervisorStrategy::Stop
 }
@@ -74,18 +56,22 @@ const ALIVE_TIMEOUT: Duration = Duration::from_secs(120);
 const WRITE_TIMEOUT: Duration = Duration::from_secs(10);
 
 async fn http_actor(
-    mut ctx: actor::Context<!, ThreadLocal>,
+    ctx: actor::Context<!, ThreadLocal>,
     mut connection: http::Connection,
-    address: SocketAddr,
 ) -> io::Result<()> {
+    let address = connection.peer_addr()?;
     info!("accepted connection: source={address}");
     connection.set_nodelay(true)?;
 
     let mut read_timeout = READ_TIMEOUT;
     loop {
-        let fut = Deadline::after(&mut ctx, read_timeout, connection.next_request());
+        let fut = Deadline::after(
+            ctx.runtime_ref().clone(),
+            read_timeout,
+            connection.next_request(),
+        );
 
-        let response = match fut.await? {
+        let response = match fut.await {
             Ok(Some(request)) => {
                 info!("received request: {request:?}: source={address}");
                 route!(match request {
@@ -99,12 +85,12 @@ async fn http_actor(
             Ok(None) => return Ok(()),
             Err(err) => {
                 warn!("error reading request: {err}: source={address}");
-                err.response().with_body("Bad request".into())
+                err.response().with_body(OneshotBody::new("Bad request"))
             }
         };
 
         let write_response = connection.respond_with(response);
-        Deadline::after(&mut ctx, WRITE_TIMEOUT, write_response).await?;
+        Deadline::after(ctx.runtime_ref().clone(), WRITE_TIMEOUT, write_response).await?;
 
         // Now that we've read a single request we can wait a little for the
         // next one so that we can reuse the resources for the next request.
@@ -112,18 +98,18 @@ async fn http_actor(
     }
 }
 
-async fn index<B>(_req: Request<B>) -> Response<OneshotBody<'static>> {
-    Response::ok().with_body("Index".into())
+async fn index<B>(_req: Request<B>) -> Response<OneshotBody<&'static str>> {
+    Response::ok().with_body(OneshotBody::new("Index"))
 }
 
-async fn other_page<B>(_req: Request<B>) -> Response<OneshotBody<'static>> {
-    Response::ok().with_body("Other page!".into())
+async fn other_page<B>(_req: Request<B>) -> Response<OneshotBody<&'static str>> {
+    Response::ok().with_body(OneshotBody::new("Other page!"))
 }
 
-async fn post<B>(_req: Request<B>) -> Response<OneshotBody<'static>> {
-    Response::ok().with_body("POST".into())
+async fn post<B>(_req: Request<B>) -> Response<OneshotBody<&'static str>> {
+    Response::ok().with_body(OneshotBody::new("POST"))
 }
 
-async fn not_found<B>(_req: Request<B>) -> Response<OneshotBody<'static>> {
-    Response::not_found().with_body("Page not found".into())
+async fn not_found<B>(_req: Request<B>) -> Response<OneshotBody<&'static str>> {
+    Response::not_found().with_body(OneshotBody::new("Page not found"))
 }
