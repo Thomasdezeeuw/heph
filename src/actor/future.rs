30261629b5e7c6b9432e3113dc27b9ebda1ab604
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2023-07-14 11:07:53 +0200

    Default to RT = () in ActorFuture
    Same as what we did for actor::Context.

--- src/actor/future.rs
+++ src/actor/future.rs
@@ -19,7 +19,7 @@ use crate::supervisor::{Supervisor, SupervisorStrategy};
 /// This can be used to wrap actors into a `Future`, it automatically handles
 /// errors and panics using the provided supervisor. This can run on any runtime
 /// that can runs `Future`s.
-pub struct ActorFuture<S, NA: NewActor, RT> {
+pub struct ActorFuture<S, NA: NewActor, RT = ()> {
     /// The actor's supervisor used to determine what to do when the actor, or
     /// the [`NewActor`] implementation, returns an error.
     supervisor: S,
