34bac3ff192ca98381e4075a2af9c8158ed41e75
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2023-07-18 16:16:48 +0200

    Rewrite sync worker
    Drops the usage of the unix pipe and instead wake the coordinator via
    it's a10::Ring, same as what we do for the workers.
    
    I've also rewritten the large main function into a new type SyncWorker.
    The main benefit here is to ensure that the waking of the coordinator
    always happens (assuming the SyncWorker is dropped properly).
    
    The old SyncWorker type is now called sync_worker::Handle, similar to
    the worker::Handle. The only difference between the two is the channel
    used by the worker (not sync) threads.

--- rt/src/sync_worker.rs
+++ rt/src/sync_worker.rs
@@ -4,88 +4,73 @@
 //! the sync actor and handles any errors it returns (by restarting the actor or
 //! stopping the thread).
 //!
-//! The [`SyncWorker`] type is a handle to the sync worker thread managed by the
-//! [coordinator]. The [`main`] function is the entry point for the sync worker
-//! thread.
+//! The [`sync_worker::Handle`] type is a handle to the sync worker thread
+//! managed by the [coordinator]. The [`main`] function is the entry point for
+//! the sync worker thread.
 //!
 //! [coordinator]: crate::coordinator
+//! [`sync_worker::Handle`]: Handle
 
-use std::io::{self, Write};
 use std::sync::Arc;
-use std::thread;
+use std::{io, thread};
 
 use heph::actor::{SyncActor, SyncContext};
 use heph::actor_ref::ActorRef;
 use heph::supervisor::{SupervisorStrategy, SyncSupervisor};
 use heph_inbox::{self as inbox, ReceiverConnected};
 use log::trace;
-use mio::{unix, Interest, Registry, Token};
 
 use crate::spawn::options::SyncActorOptions;
 use crate::trace;
 use crate::{self as rt, shared};
 
+/// Start a new thread that runs a synchronous actor.
+pub(crate) fn start<S, A>(
+    id: usize,
+    supervisor: S,
+    actor: A,
+    arg: A::Argument,
+    options: SyncActorOptions,
+    shared: Arc<shared::RuntimeInternals>,
+    trace_log: Option<trace::Log>,
+) -> io::Result<(Handle, ActorRef<A::Message>)>
+where
+    S: SyncSupervisor<A> + Send + 'static,
+    A: SyncActor<RuntimeAccess = rt::Sync> + Send + 'static,
+    A::Message: Send + 'static,
+    A::Argument: Send + 'static,
+{
+    let (inbox, sender, ..) = inbox::Manager::new_small_channel();
+    let actor_ref = ActorRef::local(sender);
+    let thread_name = options
+        .take_name()
+        .unwrap_or_else(|| format!("Sync actor {id}"));
+    let handle = thread::Builder::new().name(thread_name).spawn(move || {
+        #[rustfmt::skip]
+        let worker = SyncWorker { id, supervisor, actor, inbox, shared, trace_log };
+        worker.run(arg)
+    })?;
+    Ok((Handle { id, handle }, actor_ref))
+}
+
 /// Handle to a synchronous worker.
 #[derive(Debug)]
-pub(crate) struct SyncWorker {
+pub(crate) struct Handle {
     /// Unique id among all threads in the `Runtime`.
     id: usize,
     /// Handle for the actual thread.
     handle: thread::JoinHandle<()>,
-    /// Sending half of the Unix pipe, used to communicate with the thread.
-    sender: unix::pipe::Sender,
 }
 
-impl SyncWorker {
-    /// Start a new thread that runs a synchronous actor.
-    pub(crate) fn start<S, A>(
-        id: usize,
-        supervisor: S,
-        actor: A,
-        arg: A::Argument,
-        options: SyncActorOptions,
-        rt: Arc<shared::RuntimeInternals>,
-        trace_log: Option<trace::Log>,
-    ) -> io::Result<(SyncWorker, ActorRef<A::Message>)>
-    where
-        S: SyncSupervisor<A> + Send + 'static,
-        A: SyncActor<RuntimeAccess = rt::Sync> + Send + 'static,
-        A::Message: Send + 'static,
-        A::Argument: Send + 'static,
-    {
-        unix::pipe::new().and_then(|(sender, receiver)| {
-            let (manager, send, ..) = inbox::Manager::new_small_channel();
-            let actor_ref = ActorRef::local(send);
-            let thread_name = options
-                .take_name()
-                .unwrap_or_else(|| format!("Sync actor {id}"));
-            thread::Builder::new()
-                .name(thread_name)
-                .spawn(move || main(id, supervisor, actor, arg, manager, receiver, rt, trace_log))
-                .map(|handle| (SyncWorker { id, handle, sender }, actor_ref))
-        })
-    }
-
+impl Handle {
     /// Return the worker's id.
     pub(crate) const fn id(&self) -> usize {
         self.id
     }
 
-    /// Registers the sending end of the Unix pipe used to communicate with the
-    /// thread. Uses the [`id`] as [`Token`].
-    ///
-    /// [`id`]: SyncWorker::id
-    pub(crate) fn register(&mut self, registry: &Registry) -> io::Result<()> {
-        registry.register(&mut self.sender, Token(self.id), Interest::WRITABLE)
-    }
-
-    /// Checks if the `SyncWorker` is alive.
-    pub(crate) fn is_alive(&self) -> bool {
-        match (&self.sender).write(&[]) {
-            Ok(..) => true,
-            Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => true,
-            Err(..) => false,
-        }
+    /// See [`thread::JoinHandle::is_finished`].
+    pub(crate) fn is_finished(&self) -> bool {
+        self.handle.is_finished()
     }
 
     /// See [`thread::JoinHandle::join`].
@@ -100,79 +85,81 @@ impl SyncWorker {
     }
 }
 
-/// Run a synchronous actor worker thread.
-#[allow(clippy::too_many_arguments)]
-fn main<S, A>(
+/// Worker that runs a single synchronous actor.
+struct SyncWorker<S: SyncSupervisor<A>, A: SyncActor<RuntimeAccess = rt::Sync>> {
     id: usize,
-    mut supervisor: S,
+    supervisor: S,
     actor: A,
-    mut arg: A::Argument,
     inbox: inbox::Manager<A::Message>,
-    receiver: unix::pipe::Receiver,
-    rt: Arc<shared::RuntimeInternals>,
-    mut trace_log: Option<trace::Log>,
-) where
-    S: SyncSupervisor<A> + 'static,
-    A: SyncActor<RuntimeAccess = rt::Sync>,
-{
-    let thread = thread::current();
-    let name = thread.name().unwrap();
-    trace!(sync_worker_id = id, name = name; "running synchronous actor");
-    loop {
-        let timing = trace::start(&trace_log);
-        let receiver = inbox.new_receiver().unwrap_or_else(inbox_failure);
-        let rt = rt::Sync::new(rt.clone(), trace_log.clone());
-        let ctx = SyncContext::new(receiver, rt);
-        trace::finish_rt(
-            trace_log.as_mut(),
-            timing,
-            "setting up synchronous actor",
-            &[],
-        );
+    shared: Arc<shared::RuntimeInternals>,
+    trace_log: Option<trace::Log>,
+}
 
-        let timing = trace::start(&trace_log);
-        let res = actor.run(ctx, arg);
-        trace::finish_rt(trace_log.as_mut(), timing, "running synchronous actor", &[]);
+impl<S: SyncSupervisor<A>, A: SyncActor<RuntimeAccess = rt::Sync>> SyncWorker<S, A> {
+    /// Run the synchronous actor.
+    fn run(mut self, mut arg: A::Argument) {
+        let thread = thread::current();
+        let name = thread.name().unwrap();
+        trace!(sync_worker_id = self.id, name = name; "running synchronous actor");
+        loop {
+            let timing = trace::start(&self.trace_log);
+            let receiver = self.inbox.new_receiver().unwrap_or_else(inbox_failure);
+            let rt = rt::Sync::new(self.shared.clone(), self.trace_log.clone());
+            let ctx = SyncContext::new(receiver, rt);
+            trace::finish_rt(
+                self.trace_log.as_mut(),
+                timing,
+                "setting up synchronous actor",
+                &[],
+            );
 
-        match res {
-            Ok(()) => break,
-            Err(err) => {
-                let timing = trace::start(&trace_log);
-                match supervisor.decide(err) {
-                    SupervisorStrategy::Restart(new_arg) => {
-                        trace!(sync_worker_id = id, name = name; "restarting synchronous actor");
-                        arg = new_arg;
-                        trace::finish_rt(
-                            trace_log.as_mut(),
-                            timing,
-                            "restarting synchronous actor",
-                            &[],
-                        );
+            let timing = trace::start(&self.trace_log);
+            let res = self.actor.run(ctx, arg);
+            trace::finish_rt(
+                self.trace_log.as_mut(),
+                timing,
+                "running synchronous actor",
+                &[],
+            );
+
+            match res {
+                Ok(()) => break,
+                Err(err) => {
+                    let timing = trace::start(&self.trace_log);
+                    match self.supervisor.decide(err) {
+                        SupervisorStrategy::Restart(new_arg) => {
+                            trace!(sync_worker_id = self.id, name = name; "restarting synchronous actor");
+                            arg = new_arg;
+                            trace::finish_rt(
+                                self.trace_log.as_mut(),
+                                timing,
+                                "restarting synchronous actor",
+                                &[],
+                            );
+                        }
+                        SupervisorStrategy::Stop => {
+                            trace::finish_rt(
+                                self.trace_log.as_mut(),
+                                timing,
+                                "stopping synchronous actor",
+                                &[],
+                            );
+                            break;
+                        }
+                        _ => unreachable!(),
                     }
-                    SupervisorStrategy::Stop => {
-                        trace::finish_rt(
-                            trace_log.as_mut(),
-                            timing,
-                            "stopping synchronous actor",
-                            &[],
-                        );
-                        break;
-                    }
-                    _ => unreachable!(),
                 }
             }
         }
+        trace!(sync_worker_id = self.id, name = name; "stopping synchronous actor");
     }
+}
 
-    trace!(sync_worker_id = id, name = name; "stopping synchronous actor");
-    // First drop all values as this might take an arbitrary time.
-    drop(actor);
-    drop(supervisor);
-    drop(inbox);
-    drop(rt);
-    drop(trace_log);
-    // After dropping all values let the coordinator know we're done.
-    drop(receiver);
+impl<S: SyncSupervisor<A>, A: SyncActor<RuntimeAccess = rt::Sync>> Drop for SyncWorker<S, A> {
+    fn drop(&mut self) {
+        // Wake the coordinator forcing it check if the sync workers are still alive.
+        self.shared.wake_coordinator();
+    }
 }
 
 /// Called when we can't create a new receiver for the sync actor.
