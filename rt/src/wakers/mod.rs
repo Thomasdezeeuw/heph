1807524620b629fbe841d6a6e5739bafc5f696d8
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2023-07-21 18:54:54 +0200

    Remove usage of ring_waker
    Only the coordinator used it for getting woken up from process signals.
    However process signals already woke up the a10::Ring, so this caused
    double wake ups.

--- rt/src/wakers/mod.rs
+++ rt/src/wakers/mod.rs
@@ -5,7 +5,6 @@
 //! The implementation is fairly simple. All it does is set a bit in an
 //! [`AtomicBitMap`] contained in an [`Arc`].
 
-pub(crate) mod ring_waker;
 pub(crate) mod shared;
 #[cfg(test)]
 mod tests;
