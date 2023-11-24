2d248eb6423bd84f381e1184282713355349fe52
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2023-11-25 00:25:44 +0100

    Add 20 more Signals
    This is pretty much all of them at the moment, that can be caught at
    least.

--- rt/src/signal.rs
+++ rt/src/signal.rs
@@ -46,7 +46,7 @@ pub enum Signal {
     ///
     /// Corresponds to POSIX signal `SIGTERM`.
     Terminate,
-    /// Terminal quit signal.
+    /// Quit signal.
     ///
     /// This signal is received when the process is requested to quit and
     /// perform a core dump.
@@ -66,48 +66,261 @@ pub enum Signal {
     /// The runtime will output various metrics about itself when it receives
     /// this signal.
     User2,
+    /// Child signal.
+    ///
+    /// This signal is sent to a process when a child process terminates, is
+    /// stopped, or resumes after being stopped.
+    ///
+    /// Corresponds to POSIX signal `SIGCHLD`.
+    Child,
+    /// Alarm signal.
+    ///
+    /// This signal signals is sent to a process when a time limit is reached.
+    /// This alarm is based on real or clock time.
+    ///
+    /// Also see [`Signal::VirtualAlarm`] and  [`Signal::Profile`].
+    ///
+    /// Corresponds to POSIX signal `SIGALRM`.
+    Alarm,
+    /// Virtual timer alarm signal.
+    ///
+    /// This signal signals is sent to a process when a time limit is reached.
+    /// This alarm is based on CPU time.
+    ///
+    /// Also see [`Signal::Alarm`] and [`Signal::Profile`].
+    ///
+    /// Corresponds to POSIX signal `SIGVTALRM`.
+    VirtualAlarm,
+    /// Profiling timer signal.
+    ///
+    /// This signal signals is sent to a process when a time limit is reached.
+    /// This alarm is based on CPU time used by the process and the system
+    /// itself.
+    ///
+    /// Also see [`Signal::Alarm`] and [`Signal::VirtualAlarm`].
+    ///
+    /// Corresponds to POSIX signal `SIGPROF`.
+    Profile,
+    /// Continue signal.
+    ///
+    /// This signal sent to a process to continue (restart) after being
+    /// previously paused by the `SIGSTOP` (stop) or `SIGTSTP` signal.
+    ///
+    /// Corresponds to POSIX signal `SIGCONT`.
+    Continue,
+    /// Hangup signal.
+    ///
+    /// This signal is sent to a process when its controlling terminal is
+    /// closed. In modern systems, this signal usually means that the
+    /// controlling pseudo or virtual terminal has been closed.
+    ///
+    /// Corresponds to POSIX signal `SIGHUP`.
+    Hangup,
+    /// Window change signal.
+    ///
+    /// This signal is sent to a process when its controlling terminal changes
+    /// its size.
+    ///
+    /// Corresponds to POSIX signal `SIGWINCH`.
+    WindowChange,
+    /// Exceeded CPU signal.
+    ///
+    /// This signal is sent to a process when it has used up the CPU for a
+    /// duration that exceeds a certain predetermined user-settable value.
+    ///
+    /// Corresponds to POSIX signal `SIGXCPU`.
+    ExceededCpu,
+    /// Excess file size signal.
+    ///
+    /// This signal is sent to a process when it grows a file that exceeds the
+    /// maximum allowed size.
+    ///
+    /// Corresponds to POSIX signal `SIGXFSZ`.
+    ExcessFileSize,
+    /// Pipe signal.
+    ///
+    /// This signal is sent to a process when it attempts to write to a pipe
+    /// without a process connected to the other end.
+    ///
+    /// Corresponds to POSIX signal `SIGPIPE`.
+    Pipe,
+    /// Urgent signal.
+    ///
+    /// This signal is sent to a process when a socket has urgent or out-of-band
+    /// data available to read.
+    ///
+    /// Corresponds to POSIX signal `SIGURG`.
+    Urgent,
+    /// Bad system call signal.
+    ///
+    /// This signal is sent to a process when it passes a bad argument to a
+    /// system call. This can also be received by applications violating the
+    /// Linux Seccomp security rules configured to restrict them.
+    ///
+    /// Corresponds to POSIX signal `SIGSYS`.
+    BadSystemCall,
+    /// Trap signal.
+    ///
+    /// This signal is sent to a process when an exception (or trap) occurs: a
+    /// condition that a debugger has requested to be informed of.
+    ///
+    /// Corresponds to POSIX signal `SIGTRAP`.
+    Trap,
+    /// Abort signal.
+    ///
+    /// This signal is sent to a process to tell it to abort, i.e. to terminate.
+    /// This signal can also indicate that the CPU has executed an explicit
+    /// "trap" instruction (without a defined function), or an unimplemented
+    /// instruction (when emulation is unavailable).
+    ///
+    /// Corresponds to POSIX signal `SIGABRT`.
+    Abort,
+    /// Illegal signal.
+    ///
+    /// This signal is sent to a process when it attempts to execute an illegal,
+    /// malformed, unknown, or privileged instruction.
+    ///
+    /// Corresponds to POSIX signal `SIGILL`.
+    Illegal,
+    /// Segmentation violation signal.
+    ///
+    /// This signal is sent to a process when it makes an invalid virtual memory
+    /// reference, or segmentation fault.
+    ///
+    /// Corresponds to POSIX signal `SIGSEGV`.
+    SegmentationViolation,
+    /// Bus signal.
+    ///
+    /// This signal is sent to a process when it causes a bus error. The
+    /// conditions that lead to the signal being sent are, for example,
+    /// incorrect memory access alignment or non-existent physical address.
+    ///
+    /// Corresponds to POSIX signal `SIGBUS`.
+    Bus,
+    /// Floating point error signal.
+    ///
+    /// This signal is sent to a process when an exceptional (but not
+    /// necessarily erroneous) condition has been detected in the floating point
+    /// or integer arithmetic hardware. This may include division by zero,
+    /// floating point underflow or overflow, integer overflow, an invalid
+    /// operation or an inexact computation. Behaviour may differ depending on
+    /// hardware.
+    ///
+    /// Corresponds to POSIX signal `SIGFPE`.
+    FloatingPointError,
+    /// Terminal input for background process signal.
+    ///
+    /// This signal is sent to a process when it attempts to read from the tty
+    /// while in the background.
+    ///
+    /// Corresponds to POSIX signal `SIGTTIN`.
+    TerminalInputBackground,
+    /// Terminal output for background process signal.
+    ///
+    /// This signal is sent to a process when it attempts to write to the tty
+    /// while in the background.
+    ///
+    /// Corresponds to POSIX signal `SIGTTOU`.
+    TerminalOutputBackground,
 }
 
 impl Signal {
     /// All signals supported by [`Signal`].
-    pub(crate) const ALL: [Signal; 5] = [
+    pub(crate) const ALL: [Signal; 25] = [
         Signal::Interrupt,
         Signal::Terminate,
         Signal::Quit,
         Signal::User1,
         Signal::User2,
+        Signal::Child,
+        Signal::Alarm,
+        Signal::VirtualAlarm,
+        Signal::Profile,
+        Signal::Continue,
+        Signal::Hangup,
+        Signal::WindowChange,
+        Signal::ExceededCpu,
+        Signal::ExcessFileSize,
+        Signal::Pipe,
+        Signal::Urgent,
+        Signal::BadSystemCall,
+        Signal::Trap,
+        Signal::Abort,
+        Signal::Illegal,
+        Signal::SegmentationViolation,
+        Signal::Bus,
+        Signal::FloatingPointError,
+        Signal::TerminalInputBackground,
+        Signal::TerminalOutputBackground,
     ];
 
     /// Turns a signal number into a `Signal`. Returns `None` if we don't have a
     /// variant for the signal number.
-    pub(crate) fn from_signo(signo: libc::c_int) -> Option<Signal> {
+    pub(crate) const fn from_signo(signo: libc::c_int) -> Option<Signal> {
         Some(match signo {
             libc::SIGINT => Signal::Interrupt,
             libc::SIGTERM => Signal::Terminate,
             libc::SIGQUIT => Signal::Quit,
             libc::SIGUSR1 => Signal::User1,
             libc::SIGUSR2 => Signal::User2,
+            libc::SIGCHLD => Signal::Child,
+            libc::SIGALRM => Signal::Alarm,
+            libc::SIGVTALRM => Signal::VirtualAlarm,
+            libc::SIGPROF => Signal::Profile,
+            libc::SIGCONT => Signal::Continue,
+            libc::SIGHUP => Signal::Hangup,
+            libc::SIGWINCH => Signal::WindowChange,
+            libc::SIGXCPU => Signal::ExceededCpu,
+            libc::SIGXFSZ => Signal::ExcessFileSize,
+            libc::SIGPIPE => Signal::Pipe,
+            libc::SIGURG => Signal::Urgent,
+            libc::SIGSYS => Signal::BadSystemCall,
+            libc::SIGTRAP => Signal::Trap,
+            libc::SIGABRT => Signal::Abort,
+            libc::SIGILL => Signal::Illegal,
+            libc::SIGSEGV => Signal::SegmentationViolation,
+            libc::SIGBUS => Signal::Bus,
+            libc::SIGFPE => Signal::FloatingPointError,
+            libc::SIGTTIN => Signal::TerminalInputBackground,
+            libc::SIGTTOU => Signal::TerminalOutputBackground,
             _ => return None,
         })
     }
 
     /// Returns the signal as signal number.
-    pub(crate) fn to_signo(self) -> libc::c_int {
+    pub(crate) const fn to_signo(self) -> libc::c_int {
         match self {
             Signal::Interrupt => libc::SIGINT,
             Signal::Terminate => libc::SIGTERM,
             Signal::Quit => libc::SIGQUIT,
             Signal::User1 => libc::SIGUSR1,
             Signal::User2 => libc::SIGUSR2,
+            Signal::Child => libc::SIGCHLD,
+            Signal::Alarm => libc::SIGALRM,
+            Signal::VirtualAlarm => libc::SIGVTALRM,
+            Signal::Profile => libc::SIGPROF,
+            Signal::Continue => libc::SIGCONT,
+            Signal::Hangup => libc::SIGHUP,
+            Signal::WindowChange => libc::SIGWINCH,
+            Signal::ExceededCpu => libc::SIGXCPU,
+            Signal::ExcessFileSize => libc::SIGXFSZ,
+            Signal::Pipe => libc::SIGPIPE,
+            Signal::Urgent => libc::SIGURG,
+            Signal::BadSystemCall => libc::SIGSYS,
+            Signal::Trap => libc::SIGTRAP,
+            Signal::Abort => libc::SIGABRT,
+            Signal::Illegal => libc::SIGILL,
+            Signal::SegmentationViolation => libc::SIGSEGV,
+            Signal::Bus => libc::SIGBUS,
+            Signal::FloatingPointError => libc::SIGFPE,
+            Signal::TerminalInputBackground => libc::SIGTTIN,
+            Signal::TerminalOutputBackground => libc::SIGTTOU,
         }
     }
 
     /// Whether or not the `Signal` is considered a "stopping" signal.
     pub(crate) const fn should_stop(self) -> bool {
-        match self {
-            Signal::Interrupt | Signal::Terminate | Signal::Quit => true,
-            Signal::User1 | Signal::User2 => false,
-        }
+        matches!(self, Signal::Interrupt | Signal::Terminate | Signal::Quit)
     }
 
     /// Returns a human readable name for the signal.
@@ -118,6 +331,26 @@ impl Signal {
             Signal::Quit => "quit",
             Signal::User1 => "user-1",
             Signal::User2 => "user-2",
+            Signal::Child => "child",
+            Signal::Alarm => "alarm",
+            Signal::VirtualAlarm => "virtual-alarm",
+            Signal::Profile => "profile",
+            Signal::Continue => "continue",
+            Signal::Hangup => "handup",
+            Signal::WindowChange => "window-change",
+            Signal::ExceededCpu => "exceeded-cpu",
+            Signal::ExcessFileSize => "excess-file-size",
+            Signal::Pipe => "pipe",
+            Signal::Urgent => "urgent",
+            Signal::BadSystemCall => "bad-syscall",
+            Signal::Trap => "trap",
+            Signal::Abort => "abort",
+            Signal::Illegal => "illegal",
+            Signal::SegmentationViolation => "segfault",
+            Signal::Bus => "bus",
+            Signal::FloatingPointError => "floating-point-error",
+            Signal::TerminalInputBackground => "terminal-input",
+            Signal::TerminalOutputBackground => "terminal-output",
         }
     }
 
@@ -129,6 +362,26 @@ impl Signal {
             Signal::Quit => "SIGQUIT",
             Signal::User1 => "SIGUSR1",
             Signal::User2 => "SIGUSR2",
+            Signal::Child => "SIGCHLD",
+            Signal::Alarm => "SIGALRM",
+            Signal::VirtualAlarm => "SIGVTALRM",
+            Signal::Profile => "SIGPROF",
+            Signal::Continue => "SIGCONT",
+            Signal::Hangup => "SIGHUP",
+            Signal::WindowChange => "SIGWINCH",
+            Signal::ExceededCpu => "SIGXCPU",
+            Signal::ExcessFileSize => "SIGXFSZ",
+            Signal::Pipe => "SIGPIPE",
+            Signal::Urgent => "SIGURG",
+            Signal::BadSystemCall => "SIGSYS",
+            Signal::Trap => "SIGTRAP",
+            Signal::Abort => "SIGABRT",
+            Signal::Illegal => "SIGILL",
+            Signal::SegmentationViolation => "SIGSEGV",
+            Signal::Bus => "SIGBUS",
+            Signal::FloatingPointError => "SIGFPE",
+            Signal::TerminalInputBackground => "SIGTTIN",
+            Signal::TerminalOutputBackground => "SIGTTOU",
         }
     }
 }
