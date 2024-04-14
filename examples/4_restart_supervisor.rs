2629a0244211fcc948e909ecbf0a645e346fa865
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-04-14 17:15:38 +0200

    Use ActorFuture and SyncActorRunner in example 4
    Instead of the heph-rt crate.

--- examples/4_restart_supervisor.rs
+++ examples/4_restart_supervisor.rs
@@ -1,55 +1,47 @@
-#![feature(never_type)]
-
-use std::thread::sleep;
 use std::time::Duration;
 
 use heph::actor::{self, actor_fn};
-use heph::{restart_supervisor, sync};
-use heph_rt::spawn::{ActorOptions, SyncActorOptions};
-use heph_rt::{self as rt, Runtime, RuntimeRef, ThreadLocal};
+use heph::future::ActorFuture;
+use heph::supervisor::restart_supervisor;
+use heph::sync::{self, SyncActorRunner};
 
-fn main() -> Result<(), rt::Error> {
+mod runtime;
+
+fn main() {
     // Enable logging.
     std_logger::Config::logfmt().init();
 
-    let mut runtime = Runtime::setup().build()?;
+    // See example 1 Hell World for more information on starting and running
+    // asynchronous actors.
+    let arg = "Hello, World!";
+    let supervisor = PrintSupervisor::new(arg);
+    let (future, _) = ActorFuture::new(supervisor, actor_fn(print_actor), arg).unwrap();
 
-    let arg = "Hello world!".to_owned();
-    let supervisor = PrintSupervisor::new(arg.clone());
-    let options = SyncActorOptions::default();
-    runtime.spawn_sync_actor(supervisor, actor_fn(sync_print_actor), arg, options)?;
+    // We run our `future` on our runtime.
+    runtime::block_on(future);
 
-    // NOTE: this is only here to make the test pass.
-    sleep(Duration::from_millis(100));
-
-    runtime.run_on_workers(|mut runtime_ref: RuntimeRef| -> Result<(), !> {
-        let options = ActorOptions::default();
-        let arg = "Hello world!".to_owned();
-        let supervisor = PrintSupervisor::new(arg.clone());
-        runtime_ref.spawn_local(supervisor, actor_fn(print_actor), arg, options);
-        Ok(())
-    })?;
-
-    runtime.start()
+    // See example 3 Sync Actors for more information on starting and running
+    // synchronous actors.
+    let supervisor = PrintSupervisor::new(arg);
+    let (sync_actor, _) = SyncActorRunner::new(supervisor, actor_fn(sync_print_actor));
+    sync_actor.run(arg);
 }
 
-// Create a restart supervisor for the [`print_actor`].
+// Create a restart supervisor for the actors below.
 restart_supervisor!(
-    PrintSupervisor,
-    "print actor",
-    String,
-    5,
-    Duration::from_secs(5),
-    ": actor message '{}'",
-    args,
+    PrintSupervisor,         // Name of the supervisor type.
+    "print actor",           // Name of the actor.
+    &'static str,            // Argument for the actor.
+    5,                       // Maximum number of restarts.
+    Duration::from_secs(30), // Time to reset the max. reset counter.
 );
 
 /// A very bad printing actor.
-async fn print_actor(_: actor::Context<(), ThreadLocal>, msg: String) -> Result<(), String> {
+async fn print_actor(_: actor::Context<()>, msg: &'static str) -> Result<(), String> {
     Err(format!("can't print message '{msg}'"))
 }
 
 /// A very bad synchronous printing actor.
-fn sync_print_actor<RT>(_: sync::Context<String, RT>, msg: String) -> Result<(), String> {
+fn sync_print_actor(_: sync::Context<()>, msg: &'static str) -> Result<(), String> {
     Err(format!("can't print message synchronously '{msg}'"))
 }
