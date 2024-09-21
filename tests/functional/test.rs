924b52ad162a4882139f5297b23b0d938c593ebd
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-09-21 11:00:15 +0200

    Add tests for PanicSupervisor

--- tests/functional/test.rs
+++ tests/functional/test.rs
@@ -1,10 +1,55 @@
 //! Tests for the `test` module.
 
+use std::panic;
 use std::pin::Pin;
 use std::task::{self, Poll};
 
 use heph::actor::{self, actor_fn, Actor, NewActor};
-use heph::test::{size_of_actor, size_of_actor_val};
+use heph::sync::{self, SyncActor};
+use heph::test::{size_of_actor, size_of_actor_val, PanicSupervisor};
+use heph::{Supervisor, SyncSupervisor};
+
+struct Na;
+
+impl NewActor for Na {
+    type Message = !;
+    type Argument = ();
+    type Actor = A;
+    type Error = &'static str;
+    type RuntimeAccess = ();
+
+    fn new(
+        &mut self,
+        _: actor::Context<Self::Message, Self::RuntimeAccess>,
+        _: Self::Argument,
+    ) -> Result<Self::Actor, Self::Error> {
+        Ok(A)
+    }
+}
+
+impl SyncActor for Na {
+    type Message = !;
+    type Argument = ();
+    type Error = &'static str;
+    type RuntimeAccess = ();
+
+    fn run(
+        &self,
+        _: sync::Context<Self::Message, Self::RuntimeAccess>,
+        _: Self::Argument,
+    ) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+struct A;
+
+impl Actor for A {
+    type Error = &'static str;
+    fn try_poll(self: Pin<&mut Self>, _: &mut task::Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+}
 
 #[test]
 fn test_size_of_actor() {
@@ -14,37 +59,45 @@ fn test_size_of_actor() {
 
     assert_eq!(size_of_actor_val(&actor_fn(actor1)), 24);
 
-    struct Na;
-
-    impl NewActor for Na {
-        type Message = !;
-        type Argument = ();
-        type Actor = A;
-        type Error = !;
-        type RuntimeAccess = ();
-
-        fn new(
-            &mut self,
-            _: actor::Context<Self::Message, Self::RuntimeAccess>,
-            _: Self::Argument,
-        ) -> Result<Self::Actor, Self::Error> {
-            Ok(A)
-        }
-    }
-
-    struct A;
-
-    impl Actor for A {
-        type Error = !;
-        fn try_poll(
-            self: Pin<&mut Self>,
-            _: &mut task::Context<'_>,
-        ) -> Poll<Result<(), Self::Error>> {
-            Poll::Ready(Ok(()))
-        }
-    }
-
     assert_eq!(size_of::<A>(), 0);
     assert_eq!(size_of_actor::<Na>(), 0);
     assert_eq!(size_of_actor_val(&Na), 0);
+}
+
+#[test]
+#[should_panic = "error running 'A' actor: first error"]
+fn panic_supervisor_decide() {
+    Supervisor::<Na>::decide(&mut PanicSupervisor, "first error");
+}
+
+#[test]
+#[should_panic = "error restarting 'A' actor: restart error"]
+fn panic_supervisor_decide_on_restart_error() {
+    Supervisor::<Na>::decide_on_restart_error(&mut PanicSupervisor, "restart error");
+}
+
+#[test]
+#[should_panic = "error restarting 'A' actor a second time: second restart error"]
+fn panic_supervisor_second_restart_error() {
+    Supervisor::<Na>::second_restart_error(&mut PanicSupervisor, "second restart error");
+}
+
+#[test]
+#[should_panic = "original panic"]
+fn panic_supervisor_decide_on_panic() {
+    let panic = panic::catch_unwind(|| panic!("original panic")).unwrap_err();
+    Supervisor::<Na>::decide_on_panic(&mut PanicSupervisor, panic);
+}
+
+#[test]
+#[should_panic = "error running sync actor: first error"]
+fn panic_supervisor_sync_decide() {
+    SyncSupervisor::<Na>::decide(&mut PanicSupervisor, "first error");
+}
+
+#[test]
+#[should_panic = "original panic"]
+fn panic_supervisor_sync_decide_on_panic() {
+    let panic = panic::catch_unwind(|| panic!("original panic")).unwrap_err();
+    SyncSupervisor::<Na>::decide_on_panic(&mut PanicSupervisor, panic);
 }
