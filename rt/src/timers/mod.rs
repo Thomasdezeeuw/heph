c249897d703e556f1c0bcb71fdb96c03b605cfbc
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2025-08-23 22:30:03 +0200

    Add basic Timers implementation for Vec

--- rt/src/timers/mod.rs
+++ rt/src/timers/mod.rs
@@ -70,3 +70,44 @@ impl TimerToken {
         waker.data().addr() == self.0
     }
 }
+
+/// Very basic implementation, not ready for production.
+impl Timers for Vec<(Instant, task::Waker)> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+
+    fn next(&mut self) -> Option<Instant> {
+        self.first().map(|(i, _)| *i)
+    }
+
+    fn add(&mut self, deadline: Instant, waker: task::Waker) -> TimerToken {
+        let idx = match self.binary_search_by(|(i, _)| i.cmp(&deadline)) {
+            Ok(idx) | Err(idx) => idx,
+        };
+        let token = TimerToken::for_waker(&waker);
+        self.insert(idx, (deadline, waker));
+        token
+    }
+
+    fn remove(&mut self, deadline: Instant, token: TimerToken) {
+        if let Ok(idx) = self.binary_search_by(|(i, _)| i.cmp(&deadline)) {
+            if token.is_for_waker(&self[idx].1) {
+                _ = self.remove(idx);
+            }
+        }
+    }
+
+    fn expire_timers(&mut self, now: Instant) -> usize {
+        let idx = match self.binary_search_by(|(i, _)| i.cmp(&now)) {
+            Ok(idx) | Err(idx) => idx,
+        };
+
+        self.drain(..idx)
+            .map(|(_, w)| {
+                w.wake();
+                1
+            })
+            .sum()
+    }
+}
