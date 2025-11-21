567fab62f1e03854dd39cf76bb68a3d7c63dbe6c
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2025-11-21 12:23:03 +0100

    Use AsyncFd in the pipe module
    Removing the Sender and Receiver types as they are replaced by AsyncFd.

--- rt/src/pipe.rs
+++ rt/src/pipe.rs
@@ -1,14 +1,7 @@
 //! Unix pipes.
 //!
-//! To create a new pipe use the [`new`] function. `new` will return a
-//! [`Sender`] and [`Receiver`], which are two sides of the pipe.
-//!
-//! In addition to creating a new pipe it's also possible to create a pipe from
-//! a process' standard I/O when [spawning another process]. For this use
-//! [`Sender::from_child_stdin`], [`Receiver::from_child_stdout`] and
-//! [`Receiver::from_child_stderr`] methods. See the example below.
-//!
-//! [spawning another process]: std::process::Command
+//! To create a new pipe use the [`pipe`] function. It will return two
+//! [`AsyncFd`]s, the sending and receiving side.
 //!
 //! # Examples
 //!
@@ -19,15 +12,15 @@
 //! use std::io;
 //!
 //! use heph::actor;
-//! use heph_rt::io::{Read, Write};
-//! use heph_rt::{self as rt, pipe};
+//! use heph_rt::pipe;
+//! use heph_rt::{self as rt};
 //!
 //! const DATA: &[u8] = b"Hello, world!";
 //!
 //! async fn actor<RT>(ctx: actor::Context<!, RT>) -> io::Result<()>
 //!     where RT: rt::Access,
 //! {
-//!     let (sender, receiver) = pipe::new(ctx.runtime_ref())?;
+//!     let (sender, receiver) = pipe(ctx.runtime_ref())?;
 //!
 //!     // Write some data.
 //!     (&sender).write_all(DATA).await?;
@@ -51,8 +44,7 @@
 //! use std::process::{Command, Stdio};
 //!
 //! use heph::actor;
-//! use heph_rt::io::{Read, Write};
-//! use heph_rt::{self as rt, pipe};
+//! use heph_rt::{self as rt};
 //!
 //! const DATA: &[u8] = b"Hello, world!";
 //!
@@ -68,8 +60,8 @@
 //!         .spawn()?;
 //!
 //!     // Create our process standard in and out.
-//!     let stdin = pipe::Sender::from_child_stdin(ctx.runtime_ref(), process.stdin.take().unwrap())?;
-//!     let stdout = pipe::Receiver::from_child_stdout(ctx.runtime_ref(), process.stdout.take().unwrap())?;
+//!     let stdin = AsyncFd::new(process.stdin.take().unwrap().into(), ctx.runtime_ref())?;
+//!     let stdout = AsyncFd::new(process.stdout.take().unwrap().into(), ctx.runtime_ref())?;
 //!
 //!     // Write some data.
 //!     (&stdin).write_all(DATA).await?;
@@ -86,13 +78,11 @@
 //! ```
 
 use std::io;
-use std::os::fd::{AsFd, BorrowedFd, IntoRawFd, RawFd};
-use std::process::{ChildStderr, ChildStdin, ChildStdout};
+use std::os::fd::RawFd;
 
 use a10::AsyncFd;
 
 use crate::access::Access;
-use crate::io::{impl_read, impl_write};
 
 /// Create a new Unix pipe.
 ///
@@ -104,7 +94,7 @@ use crate::io::{impl_read, impl_write};
 /// commands to the child process.
 ///
 /// [`pipe(2)`]: https://pubs.opengroup.org/onlinepubs/9699919799/functions/pipe.html
-pub fn new<RT>(rt: &RT) -> io::Result<(Sender, Receiver)>
+pub fn pipe<RT>(rt: &RT) -> io::Result<[AsyncFd; 2]>
 where
     RT: Access,
 {
@@ -115,88 +105,5 @@ where
     // SAFETY: we just initialised the `fds` above.
     let r = unsafe { AsyncFd::from_raw_fd(fds[0], sq.clone()) };
     let w = unsafe { AsyncFd::from_raw_fd(fds[1], sq) };
-    Ok((Sender { fd: w }, Receiver { fd: r }))
-}
-
-/// Sending end of an Unix pipe.
-///
-/// Created by calling [`new`] or converted from [`ChildStdin`].
-#[derive(Debug)]
-pub struct Sender {
-    fd: AsyncFd,
-}
-
-impl Sender {
-    /// Convert a [`ChildStdin`] to a `Sender`.
-    pub fn from_child_stdin<RT>(rt: &RT, stdin: ChildStdin) -> io::Result<Sender>
-    where
-        RT: Access,
-    {
-        // SAFETY: `ChildStdin` is guaranteed to be a valid file descriptor.
-        let fd = unsafe { AsyncFd::from_raw_fd(stdin.into_raw_fd(), rt.sq()) };
-        Ok(Sender { fd })
-    }
-
-    /// Creates a new independently owned `Sender` that shares the same
-    /// underlying file descriptor as the existing `Sender`.
-    pub fn try_clone(&self) -> io::Result<Sender> {
-        Ok(Sender {
-            fd: self.fd.try_clone()?,
-        })
-    }
-}
-
-impl_write!(Sender, &Sender);
-
-impl AsFd for Sender {
-    fn as_fd(&self) -> BorrowedFd<'_> {
-        self.fd.as_fd()
-    }
-}
-
-/// Receiving end of an Unix pipe.
-///
-/// Created by calling [`new`] or converted from [`ChildStdout`] or
-/// [`ChildStderr`].
-#[derive(Debug)]
-pub struct Receiver {
-    fd: AsyncFd,
-}
-
-impl Receiver {
-    /// Convert a [`ChildStdout`] to a `Receiver`.
-    pub fn from_child_stdout<RT>(rt: &RT, stdout: ChildStdout) -> io::Result<Receiver>
-    where
-        RT: Access,
-    {
-        // SAFETY: `ChildStdout` is guaranteed to be a valid file descriptor.
-        let fd = unsafe { AsyncFd::from_raw_fd(stdout.into_raw_fd(), rt.sq()) };
-        Ok(Receiver { fd })
-    }
-
-    /// Convert a [`ChildStderr`] to a `Receiver`.
-    pub fn from_child_stderr<RT>(rt: &RT, stderr: ChildStderr) -> io::Result<Receiver>
-    where
-        RT: Access,
-    {
-        // SAFETY: `ChildStderr` is guaranteed to be a valid file descriptor.
-        let fd = unsafe { AsyncFd::from_raw_fd(stderr.into_raw_fd(), rt.sq()) };
-        Ok(Receiver { fd })
-    }
-
-    /// Creates a new independently owned `Receiver` that shares the same
-    /// underlying file descriptor as the existing `Receiver`.
-    pub fn try_clone(&self) -> io::Result<Receiver> {
-        Ok(Receiver {
-            fd: self.fd.try_clone()?,
-        })
-    }
-}
-
-impl_read!(Receiver, &Receiver);
-
-impl AsFd for Receiver {
-    fn as_fd(&self) -> BorrowedFd<'_> {
-        self.fd.as_fd()
-    }
+    Ok([r, w])
 }
