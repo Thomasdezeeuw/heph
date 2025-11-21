013af4e31ac710d775a5a774c779968590043e85
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2025-11-21 13:05:27 +0100

    Remove uds module
    To be replaced by A10's types.

--- rt/src/net/futures.rs
+++ rt/src/net/futures.rs
@@ -76,40 +76,6 @@ impl<'a, B: BufMutSlice<N>, const N: usize> Future for RecvNVectored<'a, B, N> {
     }
 }
 
-/// [`Future`] behind `recv_from` implementations.
-pub(crate) struct RecvFrom<'a, B, A>(pub(crate) a10::net::RecvFrom<'a, BufWrapper<B>, A>);
-
-impl<'a, B: BufMut, A: a10::net::SocketAddress> Future for RecvFrom<'a, B, A> {
-    type Output = io::Result<(B, A)>;
-
-    fn poll(self: Pin<&mut Self>, ctx: &mut task::Context<'_>) -> Poll<Self::Output> {
-        no_ring_ctx!(ctx);
-        // SAFETY: not moving the `Future`.
-        unsafe { Pin::map_unchecked_mut(self, |s| &mut s.0) }
-            .poll(ctx)
-            .map_ok(|(buf, addr, _)| (buf.0, addr))
-    }
-}
-
-/// [`Future`] behind `recv_from_vectored` implementations.
-pub(crate) struct RecvFromVectored<'a, B, A, const N: usize>(
-    pub(crate) a10::net::RecvFromVectored<'a, BufWrapper<B>, A, N>,
-);
-
-impl<'a, B: BufMutSlice<N>, A: a10::net::SocketAddress, const N: usize> Future
-    for RecvFromVectored<'a, B, A, N>
-{
-    type Output = io::Result<(B, A)>;
-
-    fn poll(self: Pin<&mut Self>, ctx: &mut task::Context<'_>) -> Poll<Self::Output> {
-        no_ring_ctx!(ctx);
-        // SAFETY: not moving the `Future`.
-        unsafe { Pin::map_unchecked_mut(self, |s| &mut s.0) }
-            .poll(ctx)
-            .map_ok(|(buf, addr, _)| (buf.0, addr))
-    }
-}
-
 /// [`Future`] behind `send` implementations.
 pub(crate) struct Send<'a, B>(pub(crate) Extractor<a10::net::Send<'a, BufWrapper<B>>>);
 
@@ -171,39 +137,5 @@ impl<'a, B: BufSlice<N>, const N: usize> Future for SendAllVectored<'a, B, N> {
         unsafe { Pin::map_unchecked_mut(self, |s| &mut s.0) }
             .poll(ctx)
             .map_ok(|buf| buf.0)
-    }
-}
-
-/// [`Future`] behind `send_to` implementations.
-pub(crate) struct SendTo<'a, B, A>(pub(crate) Extractor<a10::net::SendTo<'a, BufWrapper<B>, A>>);
-
-impl<'a, B: Buf, A: a10::net::SocketAddress> Future for SendTo<'a, B, A> {
-    type Output = io::Result<(B, usize)>;
-
-    fn poll(self: Pin<&mut Self>, ctx: &mut task::Context<'_>) -> Poll<Self::Output> {
-        no_ring_ctx!(ctx);
-        // SAFETY: not moving the `Future`.
-        unsafe { Pin::map_unchecked_mut(self, |s| &mut s.0) }
-            .poll(ctx)
-            .map_ok(|(buf, len)| (buf.0, len))
-    }
-}
-
-/// [`Future`] behind `send_to_vectored` implementations.
-pub(crate) struct SendToVectored<'a, B, A, const N: usize>(
-    pub(crate) Extractor<a10::net::SendMsg<'a, BufWrapper<B>, A, N>>,
-);
-
-impl<'a, B: BufSlice<N>, A: a10::net::SocketAddress, const N: usize> Future
-    for SendToVectored<'a, B, A, N>
-{
-    type Output = io::Result<(B, usize)>;
-
-    fn poll(self: Pin<&mut Self>, ctx: &mut task::Context<'_>) -> Poll<Self::Output> {
-        no_ring_ctx!(ctx);
-        // SAFETY: not moving the `Future`.
-        unsafe { Pin::map_unchecked_mut(self, |s| &mut s.0) }
-            .poll(ctx)
-            .map_ok(|(buf, n)| (buf.0, n))
     }
 }
