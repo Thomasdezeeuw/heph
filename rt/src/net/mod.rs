e20794bb3049a6b118343354e158c05a202e8c43
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2025-11-22 15:13:20 +0100

    Move Server{Error,Message} to net module
    So it can be reused by other implementations.

--- rt/src/net/mod.rs
+++ rt/src/net/mod.rs
@@ -5,6 +5,12 @@
 //!
 //! [`AsyncFd`]: crate::fd::AsyncFd
 
+use std::{fmt, io};
+
+use heph::messages::Terminate;
+
+use crate::Signal;
+
 mod tcp_server;
 
 #[doc(inline)]
@@ -13,4 +19,53 @@ pub use a10::net::{
     RecvFromVectored, RecvN, RecvNVectored, RecvVectored, Send, SendAll, SendAllVectored, SendMsg,
     SendTo, SetSocketOption, Shutdown, Socket, SocketAddress, SocketOption,
 };
-pub use tcp_server::{ServerError, ServerMessage, TcpServer};
+pub use tcp_server::TcpServer;
+
+/// The message type used by server actors.
+///
+/// The message implements [`From`]`<`[`Terminate`]`>` and
+/// [`TryFrom`]`<`[`Signal`]`>` for the message, allowing for graceful shutdown.
+#[derive(Debug)]
+pub struct ServerMessage {
+    // Allow for future expansion.
+    _inner: (),
+}
+
+impl From<Terminate> for ServerMessage {
+    fn from(_: Terminate) -> ServerMessage {
+        ServerMessage { _inner: () }
+    }
+}
+
+impl TryFrom<Signal> for ServerMessage {
+    type Error = ();
+
+    /// Converts [`Signal::Interrupt`], [`Signal::Terminate`] and
+    /// [`Signal::Quit`], fails for all other signals (by returning `Err(())`).
+    fn try_from(signal: Signal) -> Result<Self, Self::Error> {
+        match signal {
+            Signal::Interrupt | Signal::Terminate | Signal::Quit => {
+                Ok(ServerMessage { _inner: () })
+            }
+            _ => Err(()),
+        }
+    }
+}
+
+/// Error returned by server actors.
+#[derive(Debug)]
+pub enum ServerError<E> {
+    /// Error accepting an incoming connection.
+    Accept(io::Error),
+    /// Error creating a new actor to handle the connection.
+    NewActor(E),
+}
+
+impl<E: fmt::Display> fmt::Display for ServerError<E> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ServerError::Accept(err) => write!(f, "error accepting connection: {err}"),
+            ServerError::NewActor(err) => write!(f, "error creating new actor: {err}"),
+        }
+    }
+}
