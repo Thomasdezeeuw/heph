a936d605da9962f52dfa5ea811987e7eaba75356
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-02-13 23:54:42 +0100

    Update A10
    Adds LimitedBuf and some more helper methods to the buffer traits.

--- rt/src/io.rs
+++ rt/src/io.rs
@@ -35,6 +35,15 @@
 //!
 //! [multishot reads]: crate::fd::AsyncFd::multishot_read
 //!
+//! ## Other Buffer types
+//!
+//! If you hit an error that complains that "`&'0 T` must implement `Buf`, for
+//! any lifetime `'0`, but `Buf` is actually implemented for the type `&'static
+//! T`", [`StaticBuf`] is buffer to use to work around this.
+//!
+//! [`LimitedBuf`] exist to limit the amount of bytes read from or written to a
+//! buffer.
+//!
 //! # Working with Standard I/O Streams
 //!
 //! The [`stdin`], [`stdout`] and [`stderr`] functions provide handles to
