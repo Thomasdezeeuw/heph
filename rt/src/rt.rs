be5a6fb7d95c70cd2bcc2d9c56895201c85829ff
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-06-24 16:00:24 +0200

    Add SharedTimers trait
    Defines the behavour of the Timers implementation used for thread-safe
    processes.

--- rt/src/rt.rs
+++ rt/src/rt.rs
@@ -51,6 +51,56 @@ pub trait Timers {
     fn len(&self) -> usize;
 }
 
+/// Shared timers implementation.
+///
+/// An implementation of [`Timers`] that is shared between worker threads, used
+/// by timers that are created by thread-safe processes.
+///
+/// The methods on this trait are the same as on [`Timers`], but take a
+/// reference to self, rather than a mutable reference.
+pub trait SharedTimers {
+    /// Returns the next deadline, if any.
+    fn next_deadline(&self) -> Option<Instant>;
+
+    /// Same as [`next_deadline`], but returns it as a [`Duration`] instead. If
+    /// the next deadline is already passed this returns a duration of zero.
+    ///
+    /// [`next_deadline`]: Timers::next_deadline
+    fn until_next_deadline(&self) -> Option<Duration> {
+        self.next_deadline().map(|deadline| {
+            Instant::now()
+                .checked_duration_since(deadline)
+                .unwrap_or(Duration::ZERO)
+        })
+    }
+
+    /// Expire all timers that have elapsed based on `now`. Returns the amount
+    /// of expired timers.
+    ///
+    /// # Safety
+    ///
+    /// `now` may never go backwards between calls.
+    fn expire_timers(&self, now: Instant) -> usize;
+
+    /// Add a new deadline.
+    ///
+    /// The returned token can be used to cancel a timer, see [`remove`].
+    ///
+    /// [`remove`]: Timers::remove
+    fn add(&self, deadline: Instant, waker: task::Waker) -> TimerToken;
+
+    /// Remove a previously added deadline.
+    ///
+    /// The `token` should be one returned by [`add`] along with the same
+    /// `deadline`.
+    ///
+    /// [`add`]: Timers::add
+    fn remove(&self, deadline: Instant, token: TimerToken);
+
+    /// Returns the current total number of timers.
+    fn len(&self) -> usize;
+}
+
 /// Token used to expire a timer.
 #[derive(Copy, Clone, Debug)]
 pub struct TimerToken(usize);
