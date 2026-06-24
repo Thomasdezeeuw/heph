c82ac75c5ec4c97b98b531af678ca7b09adb42e9
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-06-24 15:08:43 +0200

    Add Timers trait
    Allows for external implementation of timers (once properly wired up).

--- rt/src/rt.rs
+++ rt/src/rt.rs
@@ -3,3 +3,66 @@
 //! This module contains the various parts that make up the Heph runtime.
 
 // TODO: maybe rename this? Not a fan of the repeating name.
+
+use std::task;
+use std::time::{Duration, Instant};
+
+/// Timers implementation.
+pub trait Timers {
+    /// Returns the next deadline, if any.
+    fn next_deadline(&mut self) -> Option<Instant>;
+
+    /// Same as [`next_deadline`], but returns it as a [`Duration`] instead. If
+    /// the next deadline is already passed this returns a duration of zero.
+    ///
+    /// [`next_deadline`]: Timers::next_deadline
+    fn until_next_deadline(&mut self) -> Option<Duration> {
+        self.next_deadline().map(|deadline| {
+            Instant::now()
+                .checked_duration_since(deadline)
+                .unwrap_or(Duration::ZERO)
+        })
+    }
+
+    /// Expire all timers that have elapsed based on `now`. Returns the amount
+    /// of expired timers.
+    ///
+    /// # Safety
+    ///
+    /// `now` may never go backwards between calls.
+    fn expire_timers(&mut self, now: Instant) -> usize;
+
+    /// Add a new deadline.
+    ///
+    /// The returned token can be used to cancel a timer, see [`remove`].
+    ///
+    /// [`remove`]: Timers::remove
+    fn add(&mut self, deadline: Instant, waker: task::Waker) -> TimerToken;
+
+    /// Remove a previously added deadline.
+    ///
+    /// The `token` should be one returned by [`add`] along with the same
+    /// `deadline`.
+    ///
+    /// [`add`]: Timers::add
+    fn remove(&mut self, deadline: Instant, token: TimerToken);
+
+    /// Returns the current total number of timers.
+    fn len(&self) -> usize;
+}
+
+/// Token used to expire a timer.
+#[derive(Copy, Clone, Debug)]
+pub struct TimerToken(usize);
+
+impl TimerToken {
+    /// Create a new timer token.
+    pub const fn new(data: usize) -> TimerToken {
+        TimerToken(data)
+    }
+
+    /// Returns the data passed to [`TimerToken::new`].
+    pub fn data(self) -> usize {
+        self.0
+    }
+}
