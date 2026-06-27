1c8ab313b36d9076d34282bfaa9d9ce9ea71d4ba
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-06-27 11:28:35 +0200

    Document the scheduler traits
    What they are and how they interact with one another.
    
    I haven't actually written the traits themselves though.

--- rt/src/setup/scheduler.rs
+++ rt/src/setup/scheduler.rs
@@ -1,1 +1,19 @@
 //! Scheduler implementation.
+//!
+//! The scheduler is responsible for managing all processes in the runtime.
+//! Keeping track of which processes are ready to run and which are waiting for
+//! external events (such as incoming connections) to happen before continuing.
+//! Furthermore it needs to determine which process to run next, i.e. ordering
+//! of the processes.
+//!
+//! This is implemented using the following traits:
+//!  * [`Scheduler`] is the data structure and algorithm that hold all
+//!    processes.
+//!  * [`Schedule`] can optionally used by a `Scheduler` to determine how to
+//!    order processes.
+//!  * [`Process`] represents a single process which can be added to the
+//!    `Scheduler`.
+//!  * [`RunnableProcess`] represents a process within the context of a
+//!    `Scheduler` that can be run.
+//!
+//! See the documentation on the trait themselves for more information.
